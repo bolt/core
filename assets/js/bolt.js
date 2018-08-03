1f4f5b91240b9c35e38872d0ca2d75202e1195b4
Bob den Otter <bobdenotter@gmail.com>
2018-08-03 17:43:47 +0200

    Adding Vue / Bulma

--- assets/js/bolt.js
+++ assets/js/bolt.js
@@ -1,6 +1,20 @@
 'use strict';
 
+// import Vue from 'vue'
+// import Buefy from 'buefy'
+// import 'buefy/lib/buefy.css'
+
+import '../css/bolt.css'
+
+// Vue.use(Buefy);
+
 // this loads jquery, but does *not* set a global $ or jQuery variable
 const $ = require('jquery');
 
 console.log('joe');
+
+// new Vue({
+//     el: '#app',
+//     template: '<App/>',
+//     components: { App }
+//   })
