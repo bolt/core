5b5e031e15733fc21ac9864abab1b9ff473772ed
Bob den Otter <bob@twokings.nl>
2018-09-19 17:06:59 +0200

    Tweaking

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -7,12 +7,19 @@ namespace Bolt\Controller;
 use Bolt\Configuration\Config;
 use League\Glide\Responses\SymfonyResponseFactory;
 use League\Glide\ServerFactory;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ImageController
 {
+    /** @var Config */
     private $config;
 
+    /**
+     * ImageController constructor.
+     *
+     * @param Config $config
+     */
     public function __construct(Config $config)
     {
         $this->config = $config;
@@ -20,8 +27,12 @@ class ImageController
 
     /**
      * @Route("/thumbs/{filename}", methods={"GET"}, name="thumbnail")
+     *
+     * @param string $filename
+     *
+     * @return StreamedResponse
      */
-    public function image(string $filename)
+    public function image(string $filename): StreamedResponse
     {
         $server = ServerFactory::create([
             'response' => new SymfonyResponseFactory(),
