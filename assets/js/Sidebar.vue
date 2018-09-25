5f902cf2b148737e502b14f47087b48959ed81f0
Néstor de Dios Fernández <nestor@twokings.nl>
2018-09-25 16:24:07 +0200

    Build Sidebar menu with real data

--- assets/js/Sidebar.vue
+++ assets/js/Sidebar.vue
@@ -3,37 +3,65 @@
     <div class="header item logo">
       <h2>Bolt</h2>
     </div>
-    <a href="/bolt/" class="active item">
-      <span class="fa-stack">
-        <i class="fas fa-square fa-stack-2x"></i>
-        <i class="fas fa-tachometer-alt fa-stack-1x"></i>
-      </span>
-     Dashboard
-    </a>
-
-    <div class="ui dropdown item">
-      <i class="dropdown icon"></i>
-      <span class="fa-stack">
-        <i class="fas fa-square fa-stack-2x"></i>
-        <i class="fas fa-flag fa-stack-1x"></i>
-      </span>
-      Settings
-      <div class="menu">
-        <div class="header">Text Size</div>
-        <a class="item">Small</a>
-        <a class="item">Medium</a>
-        <a class="item">Large</a>
+    <!-- TODO: Maybe we need to parse the data somewhere else -->
+    <template v-for="menuitem in JSON.parse(sidebarmenudata)">
+      <a :href="menuitem.link" class="item" v-if="!menuitem.contenttype" :key="menuitem.id">
+        <span class="fa-stack">
+          <i class="fas fa-square fa-stack-2x"></i>
+          <i class="fas fa-stack-1x" :class="menuitem.icon"></i>
+        </span>
+        {{ menuitem.name }}
+      </a>
+      <div class="ui dropdown item" :key="menuitem.id" v-else="">
+        <!-- {{menuitem.contenttype}} -->
+        <i class="dropdown icon"></i>
+        <span class="fa-stack">
+          <i class="fas fa-square fa-stack-2x"></i>
+          <i class="fas fa-stack-1x" :class="menuitem.icon"></i>
+        </span>
+        {{ menuitem.name }}
+        <div class="menu">
+          <!-- TODO: Print Links to latest edited record per contenttype -->
+          <div class="header">Text Size</div>
+          <a class="item">Small</a>
+          <a class="item">Medium</a>
+          <a class="item">Large</a>
+        </div>
       </div>
-    </div>
-    <a class="item" href="/bolt/users">
-      <span class="fa-stack">
-        <i class="fas fa-square fa-stack-2x"></i>
-        <i class="fas fa-users fa-stack-1x"></i>
-      </span>
-     Users
-    </a>
+    </template>
   </div>
 </template>
+
+<script>
+    import ContentAPI from './service/api/content';
+
+    export default {
+        name: 'sidebar',
+        props: [
+            'sidebarmenudata',
+        ],
+        data () {
+            return {
+                message: '',
+                loading: true,
+                records: []
+            };
+        },
+        created () {
+          // TODO: This data is already initialized somewhere else, Use it!!
+            this.records = ContentAPI.getRecords('pages')
+
+            ContentAPI.fetchRecords('pages')
+                .then( records => {
+                    this.records = records
+                })
+                .catch(error => console.log(error))
+                .finally(() => {
+                    this.loading = false
+                });
+        },
+    }
+</script>
 
 <style>
 .ui.small.vertical.menu {
@@ -57,7 +85,3 @@
     background-color: rgba(255, 255, 255, 0.2) !important;
 }
 </style>
-
-<script>
-
-</script>
