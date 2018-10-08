7d67152a171be4a09750609fe3eee17603ee00c2
Xiao Hu Tai <xiao@twokings.nl>
2018-10-08 15:22:37 +0200

    Read Bolt's `config.yaml` in Kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Bolt;
 
-use Bolt\Configuration\Config;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use Symfony\Component\Yaml\Yaml;
 
 class Kernel extends BaseKernel
 {
@@ -44,11 +45,17 @@ class Kernel extends BaseKernel
         $container->setParameter('container.dumper.inline_class_loader', true);
         $confDir = $this->getProjectDir() . '/config';
 
-        $config = new Config();
-        foreach ($config->getParameters() as $key => $value) {
+        $fileLocator = new FileLocator([ $confDir . '/bolt' ]);
+        $fileName = $fileLocator->locate('config.yaml', null, true);
+
+        $yaml = Yaml::parseFile($fileName);
+        unset($yaml['__nodes']);
+
+        $container->set('bolt.config.general', $yaml);
+
+        foreach ($this->flattenKeys($yaml) as $key => $value) {
             $container->setParameter('bolt.' . $key, $value);
         }
-        $container->set('config', $config);
 
         $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
         $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
@@ -63,5 +70,27 @@ class Kernel extends BaseKernel
         $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
         $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
+    }
+
+    /**
+     * @param array  $array
+     * @param string $prefix
+     *
+     * @return array
+     */
+    private function flattenKeys(array $array, string $prefix = ''): array
+    {
+        $result = [];
+        foreach ($array as $key => $value) {
+            if (is_int($key)) {
+                $result[trim($prefix, '.')][] = $value;
+            } elseif(is_array($value)) {
+                $result = $result + $this->flattenKeys($value, $prefix . $key . '.');
+            } else {
+                $result[$prefix . $key] = $value;
+            }
+        }
+
+        return $result;
     }
 }
