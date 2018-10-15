0e48a026b5e2a75989569adf8cb61bb5558756e4
Bob den Otter <bob@twokings.nl>
2018-10-15 19:45:02 +0200

    Working on replacing Semantic for Bootstrap

--- assets/js/Components/Topbar.vue
+++ assets/js/Components/Topbar.vue
@@ -8,15 +8,15 @@
                 <input type="text" class="form-control" placeholder="Search …" aria-label="Search …" aria-describedby="btnGroupAddon">
             </div>
 
-            <a href="/" class="btn btn-secondary nav-item">
+            <a href="/" class="btn btn-light nav-item">
                 <i class="fas fa-external-link-square-alt"></i> &nbsp; View site
             </a>
 
             <div class="btn-group nav-item" role="group">
-                <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button id="btnGroupDrop1" type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="text"><i class="fas fa-user-circle"></i> &nbsp; Admin</span>
                 </button>
-                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
                 <a href="/bolt/profile-edit" class="dropdown-item">Edit profile</a>
                 <a href="/logout" class="dropdown-item">Logout</a>
                 </div>
@@ -36,27 +36,26 @@ module.exports = {
 };
 </script>
 
-<style>
+<style lang="scss">
 .nav.btn-toolbar {
-    margin: 0 1rem 0 3rem;
-}
+    margin: 0.5rem 1rem 0 3rem;
 
-.nav-fill .nav-item {
-    flex-grow: 0;
-}
+    .nav-item {
+        flex-grow: 0;
+        margin-right: 1rem;
+    }
 
-.nav-fill .nav-item.topbar-title {
-  font-family: "Source Sans Pro", serif;
-  font-size: 22px;
-  color: #222;
-  line-height: 52px;
-  max-height: 52px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  text-align: left;
-  display: -webkit-box;
-  -webkit-line-clamp: 1;
-  -webkit-box-orient: vertical;
-  flex-grow: 1;
+    .nav-item.topbar-title {
+        font-family: "Source Sans Pro", serif;
+        font-size: 22px;
+        color: #222;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        text-align: left;
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        flex-grow: 1;
+    }
 }
 </style>
