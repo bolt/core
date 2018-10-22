3550237d73b0e3299e4c40a8dacdc755cec66ca3
Bob den Otter <bob@twokings.nl>
2018-10-22 13:30:25 +0200

    Adding placeholders for more fields

--- assets/js/bolt.js
+++ assets/js/bolt.js
@@ -1,20 +1,28 @@
 "use strict";
+
 /**
  * Vue Core | Config
  */
 import "./helpers/filters";
 // import './registerServiceWorker'
+
 /**
  * Bootstrap Javascript
  */
 import "bootstrap";
+
 /**
  * Styling
  */
 import "../scss/bolt.scss";
+
 /**
  * Vue Components
  */
 import "./Views/editor";
 import "./Views/base";
 
+// This loads jquery, And sets a global $ and jQuery variable. We should keep it,
+// for extensions / modules that require a global `$`.
+const $ = require("jquery");
+global.$ = global.jQuery = $;
