8a1343b128fe0207866ac7268bfa97da8342e1ce
Néstor de Dios Fernández <nestor@twokings.nl>
2018-10-26 16:25:03 +0200

    Add Configuration and File management submenus

--- src/Content/MenuBuilder.php
+++ src/Content/MenuBuilder.php
@@ -79,7 +79,7 @@ class MenuBuilder
                 'contenttype' => $contenttype['slug'],
                 'singleton' => $contenttype['singleton'],
                 'active' => $contenttype['slug'] === 'pages' ? true : false,
-                'records' => $this->getLatestRecords($contenttype['slug']),
+                'submenu' => $this->getLatestRecords($contenttype['slug']),
             ]]);
         }
 
@@ -89,28 +89,112 @@ class MenuBuilder
             'icon_one' => 'fa-wrench',
         ]]);
 
+        // Configuration submenu
+
         $menu->addChild('Configuration', ['uri' => 'configuration', 'extras' => [
             'name' => 'Configuration',
             'icon_one' => 'fa-flag',
             'link' => '/bolt/finder/config',
         ]]);
 
-        $menu->addChild('Content Files', ['uri' => 'content-files', 'extras' => [
-            'name' => 'Content Files',
+        $menu['Configuration']->addChild('Users &amp; Permissions', ['uri' => 'users', 'extras' => [
+            'name' => 'Users &amp; Permissions',
+            'icon_one' => 'fa-group',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        $menu['Configuration']->addChild('Main configuration', ['uri' => 'config', 'extras' => [
+            'name' => 'Main configuration',
+            'icon_one' => 'fa-cog',
+            'link' => '/bolt/editfile/config?file=/bolt/config.yaml',
+        ]]);
+
+        $menu['Configuration']->addChild('ContentTypes', ['uri' => 'contenttypes', 'extras' => [
+            'name' => 'ContentTypes',
+            'icon_one' => 'fa-paint-brush',
+            'link' => '/bolt/editfile/config?file=/bolt/contenttypes.yml',
+        ]]);
+
+        $menu['Configuration']->addChild('Taxonomy', ['uri' => 'taxonomy', 'extras' => [
+            'name' => 'Taxonomy',
+            'icon_one' => 'fa-tags',
+            'link' => '/bolt/editfile/config?file=/bolt/taxonomy.yml',
+        ]]);
+
+        $menu['Configuration']->addChild('Menu set up', ['uri' => 'menusetup', 'extras' => [
+            'name' => 'Menu set up',
+            'type' => 'separator',
+            'icon_one' => 'fa-list',
+            'link' => '/bolt/editfile/config?file=/bolt/menu.yml',
+        ]]);
+
+        $menu['Configuration']->addChild('Routing set up', ['uri' => 'routing', 'extras' => [
+            'name' => 'Routing set up',
+            'icon_one' => 'fa-random',
+            'link' => '/bolt/editfile/config?file=/bolt/routing.yml',
+        ]]);
+
+        $menu['Configuration']->addChild('Check database', ['uri' => 'database', 'extras' => [
+            'name' => 'Check database',
+            'type' => 'separator',
+            'icon_one' => 'fa-database',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        $menu['Configuration']->addChild('Clear the cache', ['uri' => 'cache', 'extras' => [
+            'name' => 'Clear the cache',
+            'icon_one' => 'fa-eraser',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        $menu['Configuration']->addChild('Change Log', ['uri' => 'else', 'extras' => [
+            'name' => 'Change Log',
+            'icon_one' => 'fa-archive',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        $menu['Configuration']->addChild('System Log', ['uri' => 'else', 'extras' => [
+            'name' => 'System Log',
+            'icon_one' => 'fa-archive',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        $menu['Configuration']->addChild('Set-up checks', ['uri' => 'else', 'extras' => [
+            'name' => 'Set-up checks',
+            'icon_one' => 'fa-support',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        $menu['Configuration']->addChild('Translations: Messages', ['uri' => 'else', 'extras' => [
+            'name' => 'Translations: Messages',
+            'type' => 'separator',
+            'icon_one' => 'fa-flag',
+            'link' => '/bolt/finder/config',
+        ]]);
+
+        // File Management submenu
+        $menu->addChild('File Management', ['uri' => 'content-files', 'extras' => [
+            'name' => 'File Management',
             'icon_one' => 'fa-flag',
             'link' => '/bolt/finder/files',
         ]]);
 
-        $menu->addChild('Theme Files', ['uri' => 'theme-files', 'extras' => [
-            'name' => 'Theme Files',
-            'icon_one' => 'fa-flag',
+        $menu['File Management']->addChild('Uploaded files', ['uri' => 'content-files', 'extras' => [
+            'name' => 'Uploaded files',
+            'icon_one' => 'fa-folder-open-o',
+            'link' => '/bolt/finder/files',
+        ]]);
+
+        $menu['File Management']->addChild('View/edit Templates', ['uri' => 'theme-files', 'extras' => [
+            'name' => 'View/edit Templates',
+            'icon_one' => 'fa-desktop',
             'link' => '/bolt/finder/themes',
         ]]);
 
-        $menu->addChild('Users', ['uri' => 'users', 'extras' => [
-            'name' => 'Users',
-            'icon_one' => 'fa-users',
-            'link' => '/bolt/users',
+        $menu->addChild('Extensions', ['uri' => 'extensions', 'extras' => [
+            'name' => 'Extensions',
+            'icon_one' => 'fa-cubes',
+            'link' => '/bolt/extensions',
         ]]);
 
         $this->stopwatch->stop('bolt.sidebar');
@@ -148,6 +232,30 @@ class MenuBuilder
         $menuData = [];
 
         foreach ($menu as $child) {
+
+            $submenu = [];
+
+            if( $child->hasChildren() ) {
+                foreach ($child->getChildren() as $submenuChild) {
+
+                    $submenu[] = [
+                        'name' => $submenuChild->getExtra('name') ?: $submenuChild->getLabel(),
+                        'singular_name' => $submenuChild->getExtra('singular_name'),
+                        'slug' => $submenuChild->getExtra('slug'),
+                        'singular_slug' => $submenuChild->getExtra('singular_slug'),
+                        'icon_one' => $submenuChild->getExtra('icon_one'),
+                        'icon_many' => $submenuChild->getExtra('icon_many'),
+                        'link' => $submenuChild->getExtra('link'),
+                        'link_new' => $submenuChild->getExtra('link_new'),
+                        'contenttype' => $submenuChild->getExtra('contenttype'),
+                        'singleton' => $submenuChild->getExtra('singleton'),
+                        'type' => $submenuChild->getExtra('type'),
+                        'active' => $submenuChild->getExtra('active'),
+                    ];
+
+                }
+            }
+
             $menuData[] = [
                 'name' => $child->getExtra('name') ?: $child->getLabel(),
                 'singular_name' => $child->getExtra('singular_name'),
@@ -161,7 +269,7 @@ class MenuBuilder
                 'singleton' => $child->getExtra('singleton'),
                 'type' => $child->getExtra('type'),
                 'active' => $child->getExtra('active'),
-                'records' => $child->getExtra('records'),
+                'submenu' => $child->hasChildren() ? $submenu : null,
             ];
         }
 
