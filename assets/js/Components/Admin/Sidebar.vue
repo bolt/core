86e24ee90ce8618e2d19f55da0627917b836a79f
Jack <jack@jacks.site>
2018-10-27 12:44:47 +0200

    fixed menu

--- assets/js/Components/Admin/Sidebar.vue
+++ assets/js/Components/Admin/Sidebar.vue
@@ -10,7 +10,7 @@
       <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
         <a  
           class="dropdown-item" 
-          v-for="(item, index) in menuLinks('Content')" 
+          v-for="(item, index) in content()" 
           :key="index" 
           :href="`/bolt/edit/${item.slug}`" 
           v-if="!item.singleton"
@@ -20,47 +20,36 @@
       </div>
     </div>
     <div>
-      <!-- loop through menu seperators -->
-      <div v-for="(item, index) in menuSeparator" :key="index">
-        <p class="admin__sidebar--separator">
-          {{item.name}}
-        </p>
-        <ul class="admin__sidebar--menu">
-          <!-- loop through menu items matching seperator name -->
-          <li v-for="(item, index) in menuLinks(item.name)" :key="index">
-              <template v-if="item.singleton">
-                <a :href="item.records[0].editlink" class="admin__sidebar--link">
-                  <i class="fas mr-2 link--icon" :class="item.icon_one"></i><span class="link--text">{{item.name}}</span>
-                </a>
-              </template>
-              <template v-else>
-                <a :href="item.link" class="admin__sidebar--link" :class="{'has-menu': item.records !== null}">
-                  <i class="fas mr-2 link--icon" :class="item.icon_one"></i><span class="link--text">{{item.name}}</span>
-                  <!-- loop through menu item records -->
-                  <template v-if="item.records !== null">
-                    <i class="fas fa-caret-right link--caret"></i>
-                    <ul class="link--menu">
-                      <!-- create new record -->
-                      <li v-if="item.contenttype !== null" class="link--create">
-                        <a :href="`/bolt/edit/${item.slug}`">
-                          <i class="fas fa-plus mr-2"></i><span>New {{item.singular_name}}</span>
-                        </a>
-                      </li>
-                      <li v-for="(record, index) in item.records" :key="index">
-                        <a :href="record.editlink">
-                          <i class="fas mr-2" :class="item.icon_one"></i><span>{{record.title}}</span>
-                        </a>
-                      </li>
-                    </ul>
-                    <!--  end loop -->
-                  </template>
-                </a>
-              </template>
-          </li>
-          <!--  end loop -->
-        </ul>
-      </div>
-      <!--  end loop -->
+      <ul class="admin__sidebar--menu">
+        <li v-for="(item, index) in menu" :key="index">
+          <p class="admin__sidebar--separator" v-if="item.type === 'separator'">
+            {{item.name}}
+          </p>
+          <a class="admin__sidebar--link" :href="item.link" v-else-if="item.singleton">
+            <i class="fas mr-2 link--icon" :class="item.icon_one"></i><span class="link--text">{{item.name}}</span>
+          </a>
+          <a :href="item.link" class="admin__sidebar--link" :class="{ 'has-menu': item.submenu !== null || item.contenttype !== null }" v-else>
+            <i class="fas mr-2 link--icon" :class="item.icon_one"></i><span class="link--text">{{item.name}}</span>
+
+            <template v-if="item.submenu !== null || item.contenttype !== null">
+              <i class="fas fa-caret-right link--caret"></i>
+              <ul class="link--menu">
+                <li v-if="item.link_new !== null" class="link--create">
+                  <a :href="item.link_new">
+                    <i class="fas fa-plus mr-2"></i><span>New {{item.singular_name}}</span>
+                  </a>
+                </li>
+                <li v-if="item.submenu !== null" v-for="(record, index) in item.submenu" :key="index">
+                  <a :href="record.editlink">
+                    <i class="fas mr-2" :class="record.icon_one"></i><span v-html="record.name"></span>
+                  </a>
+                </li>
+              </ul>
+            </template>
+
+          </a>
+        </li>
+      </ul>
     </div>
     <button class="admin__sidebar--slim" @click="slimMenu()"><i class="fas fa-arrows-alt-h"></i></button>
     <footer class="admin__sidebar--footer">
@@ -77,6 +66,7 @@ export default {
   name: "admin-sidebar",
   props: ["brand", "menu", "version"],
   created() {
+    console.log(this.menu);
     const size = localStorage.getItem('admin-sidebar-size');
     if (size !== null && size === 'slim'){
       this.size = 'slim'
@@ -105,19 +95,9 @@ export default {
         this.size = "normal"
       }
     },
-    menuLinks(type){
-      if(type === 'Content'){
-        return this.menu.filter(item => item.contenttype !== null)
-      }
-      if(type === 'Settings'){
-        return this.menu.filter(item => item.contenttype === null && item.type !== 'separator' && item.name !== 'Dashboard')
-      }
+    content(){
+      return this.menu.filter(item => item.contenttype !== null)
     }
   },
-  computed: {
-    menuSeparator(){
-      return this.menu.filter(item => item.type === 'separator')
-    },
-  }
 };
 </script>
