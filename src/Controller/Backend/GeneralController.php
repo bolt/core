39274467203c602aac1e6a991bd66b87e1438dd4
Néstor de Dios Fernández <nestor@twokings.nl>
2018-10-29 14:19:27 +0100

    Add Controller for Bolt's About page

---
+++ src/Controller/Backend/GeneralController.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Bolt\Controller\Backend;
+
+use Bolt\Controller\BaseController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GeneralController extends BaseController
+{
+    /**
+     * @Route("/about", name="bolt_about")
+     */
+    public function about()
+    {
+        return $this->renderTemplate('about/about.twig', [
+            'controller_name' => 'GeneralController',
+        ]);
+    }
+}
