04684f9d6d93e42dda0f5817a78b10f37af0e6e4
Jack <jack@jacks.site>
2018-11-02 14:18:59 +0100

    sorting table row css and skinning

--- assets/js/Components/Listing/Filter.vue
+++ assets/js/Components/Listing/Filter.vue
@@ -5,14 +5,19 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 55"><g fill-rule="nonzero"><rect width="70" height="10" rx="3"/><rect width="70" height="10" rx="3" transform="translate(0 15)"/><rect width="70" height="10" rx="3" transform="translate(0 30)"/><rect width="70" height="10" rx="3" transform="translate(0 45)"/></g></svg>
       </button>
     </li>
-    <li class="mr-3">
+    <li class="control--left">
       <button class="control--button" :class="{'is-selected': size === 'normal'}" @click="changeSize('normal')">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 55"><g fill-rule="nonzero"><rect width="70" height="15" rx="3"/><rect width="70" height="15" rx="3" transform="translate(0 20)"/><rect width="70" height="15" rx="3" transform="translate(0 40)"/></g></svg>
       </button>
     </li>
     <li>
-      <button class="control--button" :class="{'is-active': sorting}" @click="sortRows()">
+      <button class="control--button" :class="{'is-active': sorting}" @click="filterButton('sorting', 'listing-row-sorting')">
         <i class="fas fa-sort"></i>  
+      </button>
+    </li>
+    <li>
+      <button class="control--button" :class="{'is-active': selecting}" @click="filterButton('selecting', 'listing-row-selecting')">
+        <i :class="selecting ? 'fas fa-check-circle ':'far fa-circle'"></i>
       </button>
     </li>
   </ul>
@@ -30,17 +35,18 @@ module.exports = {
   data: () => {
     return {
       size: "normal",
-      sorting: false
+      sorting: false,
+      selecting: false
     };
   },
   methods:{
-    sortRows(){
-      if(this.sorting === false){
-        this.sorting = true
-        this.$root.$emit('listing-row-sorting', true);
+    filterButton(type, emit){
+      if(this[type] === false){
+        this[type] = true
+        this.$root.$emit(emit, true);
       } else {
-        this.sorting = false
-        this.$root.$emit('listing-row-sorting', false);
+        this[type] = false
+        this.$root.$emit(emit, false);
       }
     },
     changeSize(size){
--- assets/js/Components/Listing/Row.vue
+++ assets/js/Components/Listing/Row.vue
@@ -22,7 +22,26 @@
      </ul>
    </div>
    <div class="listing__row--item is-actions">
-     <a :href="`/bolt/edit/${id}`" class="btn btn-light btn-sm">Edit</a>
+     <ul class="listing--actions">
+       <li>
+          <a :href="`/bolt/edit/${id}`" class="link">
+            <div class="btn-group">
+              <button class="btn btn-secondary btn-sm" type="button">
+                <i class="far fa-edit mr-1"></i> Edit
+              </button>
+              <button type="button" class="btn btn-sm btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="sr-only">Toggle Dropdown</span>
+              </button>
+              <div class="dropdown-menu dropdown-menu-right">
+                ...
+              </div>
+            </div>
+          </a>
+       </li>
+       <li>    
+         <a :href="`/bolt/edit/${id}`" class="link"><i class="far fa-caret-square-down mr-1"></i>Quick Edit</a>
+        </li>
+     </ul>
    </div>
    <button v-if="sorting" class="listing__row--move"><i class="fas px-2 fa-equals"></i></button>
   </div>
