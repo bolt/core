58d3f4fc0736af91da19d95c4ce5abf854d75192
Bob den Otter <bob@twokings.nl>
2018-11-02 08:20:24 +0100

    Adding locales

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bolt\EventSubscriber;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } elseif ($locale = $request->query->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => array(array('onKernelRequest', 20)),
+        );
+    }
+}
---
+++ src/EventSubscriber/UserLocaleSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bolt\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+
+/**
+ * Stores the locale of the user in the session after the
+ * login. This can be used by the LocaleSubscriber afterwards.
+ */
+class UserLocaleSubscriber implements EventSubscriberInterface
+{
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    public function onInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if (null !== $user->getLocale()) {
+            $this->session->set('_locale', $user->getLocale());
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            SecurityEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin',
+        );
+    }
+}
