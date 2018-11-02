3251ac099cbe5a5defdec5127aed489f86a3dd4d
Bob den Otter <bob@twokings.nl>
2018-11-02 21:23:14 +0100

    Working on L10n

--- src/Content/MenuBuilder.php
+++ src/Content/MenuBuilder.php
@@ -10,6 +10,7 @@ use Bolt\Repository\ContentRepository;
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class MenuBuilder
 {
@@ -27,6 +28,10 @@ class MenuBuilder
 
     /** @var UrlGeneratorInterface */
     private $urlGenerator;
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
 
     /**
      * MenuBuilder constructor.
@@ -37,29 +42,32 @@ class MenuBuilder
      * @param ContentRepository     $content
      * @param UrlGeneratorInterface $urlGenerator
      */
-    public function __construct(FactoryInterface $factory, Config $config, Stopwatch $stopwatch, ContentRepository $content, UrlGeneratorInterface $urlGenerator)
+    public function __construct(FactoryInterface $factory, Config $config, Stopwatch $stopwatch, ContentRepository $content, UrlGeneratorInterface $urlGenerator, TranslatorInterface $translator)
     {
         $this->config = $config;
         $this->factory = $factory;
         $this->stopwatch = $stopwatch;
         $this->content = $content;
         $this->urlGenerator = $urlGenerator;
+        $this->translator = $translator;
     }
 
     public function createSidebarMenu()
     {
         $this->stopwatch->start('bolt.sidebar');
 
+        $t = $this->translator;
+
         $menu = $this->factory->createItem('root');
 
         $menu->addChild('Dashboard', ['uri' => 'homepage', 'extras' => [
-            'name' => 'Dashboard',
+            'name' => $t->trans('caption.dashboard'),
             'icon' => 'fa-tachometer-alt',
             'link' => '/bolt/',
         ]]);
 
         $menu->addChild('Content', ['uri' => 'content', 'extras' => [
-            'name' => 'Content',
+            'name' => $t->trans('caption.content'),
             'type' => 'separator',
             'icon' => 'fa-file',
         ]]);
@@ -83,7 +91,7 @@ class MenuBuilder
         }
 
         $menu->addChild('Settings', ['uri' => 'settings', 'extras' => [
-            'name' => 'Settings',
+            'name' => $t->trans('caption.settings'),
             'type' => 'separator',
             'icon' => 'fa-wrench',
         ]]);
@@ -91,81 +99,69 @@ class MenuBuilder
         // Configuration submenu
 
         $menu->addChild('Configuration', ['uri' => 'configuration', 'extras' => [
-            'name' => 'Configuration',
+            'name' => $t->trans('caption.configuration'),
             'icon' => 'fa-flag',
             'link' => '/bolt/finder/config',
         ]]);
 
         $menu['Configuration']->addChild('Users &amp; Permissions', ['uri' => 'users', 'extras' => [
-            'name' => 'Users &amp; Permissions',
+            'name' => $t->trans('caption.users_permissions'),
             'icon' => 'fa-group',
             'link' => '/bolt/finder/config',
         ]]);
 
         $menu['Configuration']->addChild('Main configuration', ['uri' => 'config', 'extras' => [
-            'name' => 'Main configuration',
+            'name' => $t->trans('caption.main_configuration'),
             'icon' => 'fa-cog',
             'link' => '/bolt/editfile/config?file=/bolt/config.yaml',
         ]]);
 
         $menu['Configuration']->addChild('ContentTypes', ['uri' => 'contenttypes', 'extras' => [
-            'name' => 'ContentTypes',
+            'name' => $t->trans('caption.contenttypes'),
             'icon' => 'fa-paint-brush',
             'link' => '/bolt/editfile/config?file=/bolt/contenttypes.yml',
         ]]);
 
-        $menu['Configuration']->addChild('Taxonomy', ['uri' => 'taxonomy', 'extras' => [
-            'name' => 'Taxonomy',
+        $menu['Configuration']->addChild('Taxonomies', ['uri' => 'taxonomy', 'extras' => [
+            'name' => $t->trans('caption.taxonomies'),
             'icon' => 'fa-tags',
             'link' => '/bolt/editfile/config?file=/bolt/taxonomy.yml',
         ]]);
 
         $menu['Configuration']->addChild('Menu set up', ['uri' => 'menusetup', 'extras' => [
-            'name' => 'Menu set up',
+            'name' => $t->trans('caption.menu_setup'),
             'type' => 'separator',
             'icon' => 'fa-list',
             'link' => '/bolt/editfile/config?file=/bolt/menu.yml',
         ]]);
 
         $menu['Configuration']->addChild('Routing set up', ['uri' => 'routing', 'extras' => [
-            'name' => 'Routing set up',
+            'name' => $t->trans('caption.routing_setup'),
             'icon' => 'fa-random',
             'link' => '/bolt/editfile/config?file=/bolt/routing.yml',
         ]]);
 
         $menu['Configuration']->addChild('Check database', ['uri' => 'database', 'extras' => [
-            'name' => 'Check database',
+            'name' => $t->trans('caption.check_database'),
             'type' => 'separator',
             'icon' => 'fa-database',
             'link' => '/bolt/finder/config',
         ]]);
 
         $menu['Configuration']->addChild('Clear the cache', ['uri' => 'cache', 'extras' => [
-            'name' => 'Clear the cache',
+            'name' => $t->trans('caption.clear_cache'),
             'icon' => 'fa-eraser',
             'link' => '/bolt/finder/config',
         ]]);
 
-        $menu['Configuration']->addChild('Change Log', ['uri' => 'else', 'extras' => [
-            'name' => 'Change Log',
-            'icon' => 'fa-archive',
-            'link' => '/bolt/finder/config',
-        ]]);
-
-        $menu['Configuration']->addChild('System Log', ['uri' => 'else', 'extras' => [
-            'name' => 'System Log',
-            'icon' => 'fa-archive',
-            'link' => '/bolt/finder/config',
-        ]]);
-
         $menu['Configuration']->addChild('Set-up checks', ['uri' => 'else', 'extras' => [
-            'name' => 'Set-up checks',
+            'name' => $t->trans('caption.setup_checks'),
             'icon' => 'fa-support',
             'link' => '/bolt/finder/config',
         ]]);
 
         $menu['Configuration']->addChild('Translations: Messages', ['uri' => 'else', 'extras' => [
-            'name' => 'Translations: Messages',
+            'name' => $t->trans('caption.translations'),
             'type' => 'separator',
             'icon' => 'fa-flag',
             'link' => '/bolt/finder/config',
@@ -173,25 +169,25 @@ class MenuBuilder
 
         // File Management submenu
         $menu->addChild('File Management', ['uri' => 'content-files', 'extras' => [
-            'name' => 'File Management',
+            'name' => $t->trans('caption.file_management'),
             'icon' => 'fa-flag',
             'link' => '/bolt/finder/files',
         ]]);
 
         $menu['File Management']->addChild('Uploaded files', ['uri' => 'content-files', 'extras' => [
-            'name' => 'Uploaded files',
+            'name' => $t->trans('caption.uploaded_files'),
             'icon' => 'fa-folder-open-o',
             'link' => '/bolt/finder/files',
         ]]);
 
         $menu['File Management']->addChild('View/edit Templates', ['uri' => 'theme-files', 'extras' => [
-            'name' => 'View/edit Templates',
+            'name' => $t->trans('caption.view_edit_templates'),
             'icon' => 'fa-desktop',
             'link' => '/bolt/finder/themes',
         ]]);
 
         $menu->addChild('Extensions', ['uri' => 'extensions', 'extras' => [
-            'name' => 'Extensions',
+            'name' => $t->trans('caption.extensions'),
             'icon' => 'fa-cubes',
             'link' => '/bolt/extensions',
         ]]);
