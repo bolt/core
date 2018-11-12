1c195fd9b3040f8e25f19e7a52a7386ad3eb296c
Jack <jack@jacks.site>
2018-11-12 18:02:56 +0100

    added svgo optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,37 @@ var path = require('path');
 
 Encore
 
-  // .addPlugin(new WebpackBar({
-  //   profile: Encore.isProduction() ? true:false,
-  //   minimal: false
-  // }))
+  .addPlugin(new WebpackBar({
+    profile: Encore.isProduction() ? true:false,
+    minimal: false
+  }))
 
-  // .addPlugin(new webpack.ProvidePlugin({
-  //   $bus: [path.resolve(__dirname, './assets/js/bus/'), 'default']
-  // }))
+  .addPlugin(new webpack.ProvidePlugin({
+    $bus: [path.resolve(__dirname, './assets/js/bus/'), 'default']
+  }))
+
+  .addLoader({
+    test: /\.svg$/,
+    loader: 'svgo-loader',
+    options: {
+      plugins: [
+        {removeTitle: true},
+        {removeDesc: true},
+        {cleanupAttrs: true},
+        {convertColors: {shorthex: true}},
+        {convertPathData: false},
+        {removeEmptyAttrs: true},
+        {minifyStyles: true},
+      ]
+    }
+  })
 
   .setOutputPath('public/assets/')
   .setPublicPath('/assets')
   .setManifestKeyPrefix('assets')
   .copyFiles({ from: './assets/static', to: '[path][name].[ext]' })
 
+  
   .cleanupOutputBeforeBuild()
   .enableSingleRuntimeChunk()
   .enableSourceMaps(!Encore.isProduction())
