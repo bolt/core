21b725e5d943d2065a6970d0c0e59bc166224546
Bob den Otter <bob@twokings.nl>
2018-11-16 09:22:57 +0100

    CS fixes

--- src/Controller/Backend/GeneralController.php
+++ src/Controller/Backend/GeneralController.php
@@ -22,10 +22,10 @@ class GeneralController extends BaseController
      */
     public function kitchensink()
     {
-        $this->addFlash('success', "<strong>Well done!</strong> You successfully read this important alert message.");
-        $this->addFlash('info', "<strong>Heads up!</strong> This alert needs your attention, but it's not super important.");
-        $this->addFlash('warning', "<strong>Warning!</strong> Better check yourself, you're not looking too good.");
-        $this->addFlash('danger', "<strong>Oh snap!</strong> Change a few things up and try submitting again.");
+        $this->addFlash('success', '<strong>Well done!</strong> You successfully read this important alert message.');
+        $this->addFlash('info', '<strong>Heads up!</strong> This alert needs your attention, but it\'s not super important.');
+        $this->addFlash('warning', '<strong>Warning!</strong> Better check yourself, you\'re not looking too good.');
+        $this->addFlash('danger', '<strong>Oh snap!</strong> Change a few things up and try submitting again.');
 
         return $this->renderTemplate('pages/about.twig');
     }
