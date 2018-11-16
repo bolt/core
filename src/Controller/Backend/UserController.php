da3b160a105ebcbacce4f5b1ac2b65381da36c9a
Néstor de Dios Fernández <nestor@twokings.nl>
2018-11-16 14:02:09 +0100

    Validate userame, password and email in edit profile

--- src/Controller/Backend/UserController.php
+++ src/Controller/Backend/UserController.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Bolt\Controller\Backend;
 
 use Bolt\Controller\BaseController;
-use Bolt\Form\ChangePasswordType;
-use Bolt\Form\UserType;
 use Doctrine\Common\Persistence\ObjectManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -39,36 +37,61 @@ class UserController extends BaseController
         $user = $this->getUser();
 
         return $this->renderTemplate('users/edit.twig', [
+            'usertitle' => $user->getFullName(),
             'user' => $user,
         ]);
     }
+
     /**
      * @Route("/profile-edit", methods={"POST"}, name="bolt_profile_edit_post")
      */
-    public function edit_post(Request $request, UrlGeneratorInterface $urlGenerator, ObjectManager $manager, UserPasswordEncoderInterface $encoder): Response
+    public function edit_post(Request $request, UrlGeneratorInterface $urlGenerator,
+                              ObjectManager $manager, UserPasswordEncoderInterface $encoder): Response
     {
         $user = $this->getUser();
+        $userTitle = $user->getFullName();
         $url = $urlGenerator->generate('bolt_profile_edit');
         $locale = $request->get('user')['locale'];
+        $newPassword = $request->get('password');
 
-        $newPassword = $request->get('user')['password_first'];
-        $confirmedPassword = $request->get('user')['password_second'];
-
-        $user->setFullName($request->get('user')['fullName']);
-        $user->setEmail($request->get('user')['email']);
+        $user->setFullName($request->get('fullName'));
+        $user->setEmail($request->get('email'));
         $user->setLocale($locale);
         $user->setbackendTheme($request->get('user')['backendTheme']);
 
-        if (!empty($newPassword) || !empty($confirmedPassword))
-        {
-            // Set new password
-            if($newPassword === $confirmedPassword) {
-                $user->setPassword($encoder->encodePassword($user, $newPassword));
-            } else{
-                return $this->renderTemplate('users/edit.twig', [
-                    'user' => $user,
-                ]);
-            }
+        $hasError = false;
+
+        $usernameValidateOptions = [
+            'options' => [
+                'min_range' => 1,
+            ],
+        ];
+
+        // Validate username
+        if (!filter_var(strlen($user->getFullName()), FILTER_VALIDATE_INT, $usernameValidateOptions)) {
+            $this->addFlash('danger', 'user.not_valid_username');
+            $hasError = true;
+        }
+
+        // Validate password
+        if (!empty($newPassword) && strlen($newPassword) < 6) {
+            $this->addFlash('danger', 'user.not_valid_password');
+            $hasError = true;
+        } elseif(!empty($newPassword) && strlen($newPassword) > 6) {
+            $user->setPassword($encoder->encodePassword($user, $newPassword));
+        }
+
+        // Validate email
+        if (!filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL)) {
+            $this->addFlash('danger', 'user.not_valid_email');
+            $hasError = true;
+        };
+
+        if($hasError){
+            return $this->renderTemplate('users/edit.twig', [
+                'usertitle' => $userTitle,
+                'user'     => $user,
+            ]);
         }
 
         $manager->flush();
