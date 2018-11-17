6f7e403e560559dc362cd6ae38c2a3bd322c8c01
Bob den Otter <bob@twokings.nl>
2018-11-17 16:39:09 +0100

    Create ResetPasswordController.php

---
+++ src/Controller/Backend/ResetPasswordController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Controller\Backend;
+
+use Bolt\Controller\BaseController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResetPasswordController extends BaseController
+{
+    /**
+     * @Route("/resetpassword", name="bolt_resetpassword")
+     */
+    public function omnisearch(): Response
+    {
+        $twigVars = [
+            'title' => 'Reset Password',
+            'subtitle' => 'To reset your password, if you\'ve misplaced it',
+        ];
+
+        return $this->renderTemplate('pages/placeholder.html.twig', $twigVars);
+    }
+}
