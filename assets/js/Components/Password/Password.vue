50fe4304213245606ac6499ec59c5c96ed6dce1b
Néstor de Dios Fernández <nestor@twokings.nl>
2018-11-19 15:49:01 +0100

    Add password field

---
+++ assets/js/Components/Password/Password.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="input-group-prepend">
+      <input
+        type="password"
+        :id="id"
+        :name="name"
+        :value="value"
+        class="form-control"
+      />
+
+      <i class="input-group-text fas fa-eye"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "field-password",
+  props: ["value", "name", "id"]
+};
+</script>
--- assets/js/Views/user.js
+++ assets/js/Views/user.js
@@ -3,11 +3,13 @@ import Vue from "vue";
  * Components
  */
 import Theme from "../Components/User/Theme";
-
+import Password from "../Components/Password/Password";
 
 /**
  * Register Components
  */
 Vue.component("user-theme", Theme);
+Vue.component("field-password", Password);
 
 new Vue({ el: "#user", name: "user" });
+new Vue({ el: "#field-password", name: "field-password" });
