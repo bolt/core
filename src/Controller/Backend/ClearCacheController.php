03f2e6221d45edf7878bed7f413bc454e9fdb31c
Bob den Otter <bobdenotter@gmail.com>
2018-11-20 07:53:26 +0100

    CS fixes, capture output

--- src/Controller/Backend/ClearCacheController.php
+++ src/Controller/Backend/ClearCacheController.php
@@ -1,31 +1,39 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Bolt\Controller\Backend;
 
+use Bolt\Controller\BaseController;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Bolt\Controller\BaseController;
 
 class ClearCacheController extends BaseController
 {
     /**
      * @Route("/clearcache", name="bolt_clear_cache")
      */
-    public function index( KernelInterface $kernel): Response
+    public function index(KernelInterface $kernel): Response
     {
         $application = new Application($kernel);
         $application->setAutoExit(false);
 
-        $input = new ArrayInput(array(
-            'command' => 'cache:clear'
-        ));
+        $input = new ArrayInput([
+            'command' => 'cache:clear',
+        ]);
+        $output = new BufferedOutput();
 
-        $application->run($input);
-        $this->addFlash('success', '');
+        $application->run($input, $output);
+        $this->addFlash('success', 'Cache cleared!');
 
-        return $this->render('clearcache/clearcache.html.twig');
+        $twigvars = [
+            'output' => $output->fetch(),
+        ];
+
+        return $this->render('clearcache/clearcache.html.twig', $twigvars);
     }
 }
