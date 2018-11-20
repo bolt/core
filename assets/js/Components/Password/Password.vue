bd19aec1768b9456669b90ab0d12c7f9beb95e8d
Néstor de Dios Fernández <nestor@twokings.nl>
2018-11-20 10:55:44 +0100

    Add togglePassword method to make password visible

--- assets/js/Components/Password/Password.vue
+++ assets/js/Components/Password/Password.vue
@@ -10,7 +10,7 @@
         autocomplete="new-password"
       />
 
-      <i class="input-group-text fas fa-eye"></i>
+      <i class="input-group-text fas fa-eye" @click="togglePassword"></i>
     </div>
   </div>
 </template>
@@ -18,6 +18,18 @@
 <script>
 export default {
   name: "field-password",
-  props: ["value", "name", "id"]
+  props: ["value", "name", "id"],
+  methods: {
+    togglePassword: function(event) {
+      let inputElement = event.target.previousElementSibling;
+      let inputType = inputElement.attributes.getNamedItem("type").value;
+
+      if (inputType === "password") {
+        inputElement.setAttribute("type", "text");
+      } else if (inputType === "text") {
+        inputElement.setAttribute("type", "password");
+      }
+    }
+  }
 };
 </script>
