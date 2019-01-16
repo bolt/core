739fef591542434151d3a5d82d14ed79387b2428
Bob den Otter <bob@twokings.nl>
2019-01-16 22:38:50 +0100

    Cache config files

--- src/Configuration/Config.php
+++ src/Configuration/Config.php
@@ -8,6 +8,8 @@ use Bolt\Common\Arr;
 use Bolt\Configuration\Parser\ContentTypesParser;
 use Bolt\Configuration\Parser\GeneralParser;
 use Bolt\Configuration\Parser\TaxonomyParser;
+use Symfony\Component\Cache\Simple\FilesystemCache;
+use Symfony\Component\Stopwatch\Stopwatch;
 use Tightenco\Collect\Support\Collection;
 
 class Config
@@ -18,17 +20,71 @@ class Config
     /** @var PathResolver */
     private $pathResolver;
 
-    public function __construct()
-    {
-        $this->pathResolver = new PathResolver(dirname(dirname(__DIR__)), []);
+    /** @var Stopwatch */
+    private $stopwatch;
 
-        $this->data = $this->parseConfig();
+    /** @var FilesystemCache */
+    private $cache;
+
+    /** @var array */
+    private $timestamps = [];
+
+    public function __construct(Stopwatch $stopwatch)
+    {
+        $this->stopwatch = $stopwatch;
+        $this->cache = new FilesystemCache();
+        $this->pathResolver = new PathResolver(dirname(dirname(__DIR__)), []);
+        $this->data = $this->getConfig();
+    }
+
+    private function getConfig(): Collection
+    {
+        $this->stopwatch->start('bolt.parseconfig');
+
+        if ($this->validCache()) {
+            $data = $this->getCache();
+        } else {
+            $data = $this->parseConfig();
+            $this->setCache();
+        }
+
+        $this->stopwatch->stop('bolt.parseconfig');
+
+        return $data;
+    }
+
+    private function validCache(): bool
+    {
+        if (! $this->cache->has('config_cache') || ! $this->cache->has('config_timestamps')) {
+            return false;
+        }
+
+        $timestamps = $this->cache->get('config_timestamps');
+
+        foreach ($timestamps as $filename => $timestamp) {
+            if (filemtime($filename) > $timestamp) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function getCache(): Collection
+    {
+        return $this->cache->get('config_cache');
+    }
+
+    private function setCache(): void
+    {
+        $this->cache->set('config_cache', $this->data);
+        $this->cache->set('config_timestamps', $this->timestamps);
     }
 
     /**
      * Load the configuration from the various YML files.
      */
-    public function parseConfig(): Collection
+    private function parseConfig(): Collection
     {
         $general = new GeneralParser();
 
@@ -49,7 +105,25 @@ class Config
         //'permissions' => $this->parseConfigYaml('permissions.yml'),
         //'extensions' => $this->parseConfigYaml('extensions.yml'),
 
+        $this->setTimestamps($general, $taxonomy, $contentTypes);
+
         return $config;
+    }
+
+    private function setTimestamps(...$configs): void
+    {
+        $this->timestamps = [];
+
+        foreach ($configs as $config) {
+            foreach ($config->getFilenames() as $file) {
+                $this->timestamps[$file] = filemtime($file);
+            }
+        }
+
+        $envFilename = dirname(dirname(__DIR__)) . '/.env';
+        if (file_exists($envFilename)) {
+            $this->timestamps[$envFilename] = filemtime($envFilename);
+        }
     }
 
     /**
