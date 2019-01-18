60fb3e51160963a09d402f328741ca38915d2a11
Jarek Jakubowski <egger1991@gmail.com>
2019-01-18 14:29:23 +0100

    Fix Github conflict manually

--- tests/e2e/pages/pages_overview.js
+++ tests/e2e/pages/pages_overview.js
@@ -6,18 +6,9 @@ class PagesOverviewPage extends BasePage {
 
     this.url = '/bolt/content/pages';
 
-<<<<<<< Updated upstream
-    this.edit_button = element(by.css('#listing .listing__row .listing--actions .link'));
-    this.record_title = element(by.css('#listing .listing__row .is-details a'));
-
-    // edit record
-    this.title_field = element(by.id('field-title'));
-    this.save_button = element(by.css('button[type="submit"]'));
-=======
     this.edit_button = $('#listing .listing__row .listing--actions .link');
     this.pager_next = $('nav.listing__filter nav a[rel="next"]');
     this.record_title = $('#listing .listing__row .is-details a');
->>>>>>> Stashed changes
   }
 }
 
