018aa01543e2686a87b795aa3fe321c86957f96d
Bob den Otter <bob@twokings.nl>
2019-01-26 16:08:44 +0100

    Working on translations

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,6 @@ return [
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Translation\PlatformAdapter\Loco\Bridge\Symfony\TranslationAdapterLocoBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
