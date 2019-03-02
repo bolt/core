8a331b4d7cd3e012cc7b4d90c9caccb7210c2207
Bob den Otter <bob@twokings.nl>
2019-03-02 14:33:05 +0100

    Limit length of indexed column to 191

--- src/Entity/Translation.php
+++ src/Entity/Translation.php
@@ -4,29 +4,35 @@ declare(strict_types=1);
 
 namespace Gedmo\Translatable\Entity;
 
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\Index;
-use Doctrine\ORM\Mapping\Table;
-use Doctrine\ORM\Mapping\UniqueConstraint;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Gedmo\Translatable\Entity\Translation
  *
- * @Table(
+ * @ORM\Table(
  *         name="translation",
  *         options={"row_format":"DYNAMIC"},
- *         indexes={@Index(name="translations_lookup_idx", columns={
+ *         indexes={@ORM\Index(name="translations_lookup_idx", columns={
  *             "locale", "object_class", "foreign_key"
  *         })},
- *         uniqueConstraints={@UniqueConstraint(name="lookup_unique_idx", columns={
+ *         uniqueConstraints={@ORM\UniqueConstraint(name="lookup_unique_idx", columns={
  *             "locale", "object_class", "field", "foreign_key"
  *         })}
  * )
- * @Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
+ * @ORM\Entity(repositoryClass="Gedmo\Translatable\Entity\Repository\TranslationRepository")
  */
 class Translation extends MappedSuperclass\AbstractTranslation
 {
-    /*
-     * All required columns are mapped through inherited superclass
+    /**
+     * All required columns are mapped through inherited superclass, except
+     * for $objectClass, because we need to limit the length to 191, for MySQL 5.6
+     * compatibility. See https://github.com/doctrine/orm/issues/7416
      */
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="object_class", type="string", length=191)
+     */
+    protected $objectClass;
 }
