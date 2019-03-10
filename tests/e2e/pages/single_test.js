44141a6fa75f794eca4000dbaa6f995053729202
Bob den Otter <bob@twokings.nl>
2019-03-10 10:33:15 +0100

    Add Kakunin test

---
+++ tests/e2e/pages/single_test.js
@@ -0,0 +1,22 @@
+const { BasePage } = require('kakunin');
+
+class SingleTestPage extends BasePage {
+  constructor() {
+    super();
+
+    this.url = '/page/:slug';
+
+    this.title = $('h1.title');
+    this.heading = $('h1.heading');
+
+    this.text_markup_a = $('.text_markup_a');
+    this.text_markup_b = $('.text_markup_b');
+    this.text_markup_c = $('.text_markup_c');
+
+    this.text_plain_a = $('.text_plain_a');
+    this.text_plain_b = $('.text_plain_b');
+    this.text_plain_c = $('.text_plain_c');
+  }
+}
+
+module.exports = SingleTestPage;
