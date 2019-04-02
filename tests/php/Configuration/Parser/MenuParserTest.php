0a336a011a04f521d470c3dc967473a274c22bed
Bob den Otter <bob@twokings.nl>
2019-04-02 13:57:54 +0200

    Requested changes.

--- tests/php/Configuration/Parser/MenuParserTest.php
+++ tests/php/Configuration/Parser/MenuParserTest.php
@@ -10,29 +10,35 @@ use Tightenco\Collect\Support\Collection;
 
 class MenuParserTest extends TestCase
 {
-    /** @var MenuParser */
-    private $menuParser;
-
-    protected function setUp(): void
-    {
-        $this->menuParser = new MenuParser();
-    }
-
     public function testCanParse(): void
     {
-        $config = $this->menuParser->parse();
+        $menuParser = new MenuParser();
+        $config = $menuParser->parse();
 
         $this->assertInstanceOf(Collection::class, $config);
     }
 
     public function testHasMenu(): void
     {
-        $config = $this->menuParser->parse();
+        $menuParser = new MenuParser();
+        $config = $menuParser->parse();
 
-        $this->assertCount(1, $config);
+        $this->assertCount(2, $config);
 
         $this->assertArrayHasKey('main', $config);
         $this->assertCount(4, $config['main']);
+
+        $this->assertSame('Home', $config['main'][0]['label']);
+        $this->assertSame('This is the <b>first<b> menu item.', $config['main'][0]['title']);
+        $this->assertSame('homepage', $config['main'][0]['link']);
+        $this->assertSame('homepage', $config['main'][0]['class']);
+        $this->assertNull($config['main'][0]['submenu']);
+        $this->assertSame('', $config['main'][0]['uri']);
+        $this->assertFalse($config['main'][0]['current']);
+        $this->assertArrayNotHasKey('foobar', $config['main'][0]);
+
+        $this->assertCount(4, $config['main'][1]['submenu']);
+        $this->assertSame('Sub 1', $config['main'][1]['submenu'][0]['label']);
 
         $this->assertArrayNotHasKey('foo', $config);
     }
