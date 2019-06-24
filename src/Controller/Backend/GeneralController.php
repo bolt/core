b1509cd01f7c3531f98cbd2bf7bd756219791230
Bob den Otter <bob@twokings.nl>
2019-06-24 20:39:48 +0200

    Clean up SCSS structure

--- src/Controller/Backend/GeneralController.php
+++ src/Controller/Backend/GeneralController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Bolt\Controller\Backend;
 
 use Bolt\Controller\TwigAwareController;
+use Bolt\Entity\Content;
+use Bolt\Repository\ContentRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,8 +27,11 @@ class GeneralController extends TwigAwareController implements BackendZone
     /**
      * @Route("/kitchensink", name="bolt_kitchensink")
      */
-    public function kitchensink(): Response
+    public function kitchensink(ContentRepository $content): Response
     {
+        /** @var Content $records */
+        $records = $content->findLatest(null, 4);
+
         $this->addFlash('success', '<strong>Well done!</strong> You successfully read this important alert message.');
         $this->addFlash('info', '<strong>Heads up!</strong> This alert needs your attention, but it\'s not super important.');
         $this->addFlash('warning', '<strong>Warning!</strong> Better check yourself, you\'re not looking too good.');
@@ -35,8 +40,9 @@ class GeneralController extends TwigAwareController implements BackendZone
         $twigVars = [
             'title' => 'Kitchensink',
             'subtitle' => 'To show a number of different things, on one page',
+            'records' => $records,
         ];
 
-        return $this->renderTemplate('@bolt/pages/placeholder.html.twig', $twigVars);
+        return $this->renderTemplate('@bolt/pages/kitchensink.html.twig', $twigVars);
     }
 }
