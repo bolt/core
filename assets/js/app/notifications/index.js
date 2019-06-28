5e989b17d502e751f3bf44318dbb8efcf4ff3ba4
Sebastiaan Bonardt <bonardt@gmail.com>
2019-06-28 16:16:32 +0200

    redo of layout backend, making it responsive and mobile first. MVP for now

--- assets/js/app/notifications/index.js
+++ assets/js/app/notifications/index.js
@@ -2,4 +2,19 @@ import $ from 'jquery';
 
 $(document).ready(function() {
   $('.toast').toast('show');
+  // add a js class to indicate we have JS enabled. Needs a change to either modernizr or something custom @bobdenotter
+  $('html').addClass('js');
+
+  $('.admin__sidebar').addClass('admin__sidebar--is-collapsed');
+
+  $('.admin-sidebar-toggler').click(function() {
+    if ($('.admin__sidebar').hasClass('admin__sidebar--is-collapsed')) {
+      $('.admin__sidebar').addClass('admin__sidebar--is-expanded').removeClass('admin__sidebar--is-collapsed');
+      $(this).toggleClass('is-active');
+    } else {
+      $('.admin__sidebar').addClass('admin__sidebar--is-collapsed').removeClass('admin__sidebar--is-expanded');
+      $(this).toggleClass('is-active');
+    }
+  });
+
 });
