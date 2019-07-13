0de3818ecba4b228998a3835b2042aeff400da37
Bob den Otter <bob@twokings.nl>
2019-07-13 12:54:52 +0200

    Add new temporary logo

--- assets/js/app/toolbar/Components/Toolbar.vue
+++ assets/js/app/toolbar/Components/Toolbar.vue
@@ -5,21 +5,9 @@
     role="toolbar"
   >
     <div class="toolbar-item toolbar-item__brand">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 91 79">
-        <g fill-rule="evenodd">
-          <path
-            d="M22.4439 78.14003L0 38.98236 22.75 0l45.19015.17305L90.3848 39.33448 67.63857 78.32135l-45.18714-.17305-.00752-.00827zm4.97036-7.58863l.00588.00667 35.31413.13936 17.77638-31.39679L62.96997 7.76328l-35.31649-.13935L9.87415 39.01708 27.41426 70.5514z"
-          />
-          <path
-            fill-rule="nonzero"
-            d="M31.58678 31.6h7.52066v15.04762h-7.52066z"
-          />
-          <path
-            fill-rule="nonzero"
-            d="M37.5657 35.3619h22.56198v7.52381H37.5657z"
-          />
-        </g>
-      </svg>
+
+      <img src="/assets/logos/bolt_logo_dashboard.svg" alt="⚙️ Bolt" height="28" />
+
       <ul class="toolbar-menu">
         <li><a href="https://bolt.cm/" target="_blank">Visit Bolt.cm</a></li>
         <li>
@@ -31,11 +19,11 @@
     </div>
     <div class="toolbar-item toolbar-item__site">
       <a href="/" target="_blank">
-        <i class="fas fa-globe-americas mr-2"></i>{{ labels['action.visit_site'] }}
+        <i class="fas fa-globe-americas mr-2"></i>{{ labels['action.view_site'] }}
       </a>
     </div>
     <div class="toolbar-item toolbar-item__new">
-      <i class="fas fa-magic mr-2"></i>{{ labels['action.create_new'] }}
+      <i class="fas fa-magic mr-2"></i>{{ labels['action.create_new'] }} …
       <ul class="toolbar-menu">
         <li v-for="(item, index) in createMenu" :key="index">
           <a :href="item.link_new">{{ item.singular_name }}</a>
