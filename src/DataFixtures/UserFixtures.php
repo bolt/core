cd76b23d2d220d8dd568f6181973bb4a8b1aba35
Bob den Otter <bob@twokings.nl>
2019-07-17 19:31:46 +0200

    Update UserFixtures.php

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Bolt\DataFixtures;
 
 use Bolt\Entity\User;
+use Bolt\Utils\Str;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends BaseFixture implements FixtureGroupInterface
@@ -14,9 +16,16 @@ class UserFixtures extends BaseFixture implements FixtureGroupInterface
     /** @var UserPasswordEncoderInterface */
     private $passwordEncoder;
 
+    private $append = false;
+
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
+
+        // If ran with `--append` we append users, and use random passwords for them
+        if (in_array('--append', $_SERVER['argv'])) {
+            $this->append = true;
+        }
     }
 
     public function load(ObjectManager $manager): void
@@ -56,35 +65,35 @@ class UserFixtures extends BaseFixture implements FixtureGroupInterface
             [
                 'displayname' => 'Admin',
                 'username' => 'admin',
-                'password' => 'admin%1',
+                'password' => $this->append ? Str::generatePassword(10) : 'admin%1',
                 'email' => 'admin@example.org',
                 'roles' => ['ROLE_ADMIN'],
             ],
             [
                 'displayname' => 'Gekke Henkie',
                 'username' => 'henkie',
-                'password' => 'henkie%1',
+                'password' => $this->append ? Str::generatePassword(10) : 'henkie%1',
                 'email' => 'henkie@example.org',
                 'roles' => ['ROLE_EDITOR'],
             ],
             [
                 'displayname' => 'Jane Doe',
                 'username' => 'jane_admin',
-                'password' => 'kitten',
+                'password' => $this->append ? Str::generatePassword(10) : 'jane%1',
                 'email' => 'jane_admin@example.org',
                 'roles' => ['ROLE_ADMIN'],
             ],
             [
                 'displayname' => 'Tom Doe',
                 'username' => 'tom_admin',
-                'password' => 'kitten',
+                'password' => $this->append ? Str::generatePassword(10) : 'tom%1',
                 'email' => 'tom_admin@example.org',
                 'roles' => ['ROLE_ADMIN'],
             ],
             [
                 'displayname' => 'John Doe',
                 'username' => 'john_user',
-                'password' => 'kitten',
+                'password' => $this->append ? Str::generatePassword(10) : 'john%1',
                 'email' => 'john_user@example.org',
                 'roles' => ['ROLE_USER'],
             ],
