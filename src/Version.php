f6b1d8d64c61e8cfb65fc073e94abc846d62fb7b
Bob den Otter <bob@twokings.nl>
2019-08-10 14:17:41 +0200

    Preparing release 4.0.0 beta 2.

--- src/Version.php
+++ src/Version.php
@@ -21,8 +21,8 @@ final class Version
      *   Stable      — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.0.0 beta 1';
-    public const CODENAME = 'First hush-hush beta release';
+    public const VERSION = '4.0.0 beta 2';
+    public const CODENAME = 'The second beta release';
 
     /**
      * Whether this release is a stable one.
