8236e7b331be8e6f28b6d7742f443c860af517dc
Bob den Otter <bob@twokings.nl>
2019-09-28 17:29:51 +0200

    Making `type: select` fields work with selecting from a ContentType.

--- src/Twig/RecordExtension.php
+++ src/Twig/RecordExtension.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Bolt\Twig;
 
+use Bolt\Configuration\Config;
+use Bolt\Configuration\Content\ContentType;
 use Bolt\Entity\Content;
 use Bolt\Entity\Field;
+use Bolt\Repository\ContentRepository;
 use Bolt\Repository\TaxonomyRepository;
 use Doctrine\Common\Collections\Collection;
 use Pagerfanta\Pagerfanta;
@@ -23,16 +26,24 @@ use Twig\TwigFunction;
  */
 class RecordExtension extends AbstractExtension
 {
+    /** @var ContentRepository */
+    private $contentRepository;
+
     /** @var TaxonomyRepository */
     private $taxonomyRepository;
 
     /** @var Request */
     private $request;
 
-    public function __construct(TaxonomyRepository $taxonomyRepository, RequestStack $requestStack)
+    /** @var Config */
+    private $config;
+
+    public function __construct(ContentRepository $contentRepository, TaxonomyRepository $taxonomyRepository, RequestStack $requestStack, Config $config)
     {
+        $this->contentRepository = $contentRepository;
         $this->taxonomyRepository = $taxonomyRepository;
         $this->request = $requestStack->getCurrentRequest();
+        $this->config = $config;
     }
 
     /**
@@ -46,7 +57,7 @@ class RecordExtension extends AbstractExtension
         return [
             new TwigFunction('list_templates', [$this, 'getListTemplates']),
             new TwigFunction('pager', [$this, 'pager'], $env + $safe),
-            new TwigFunction('selectoptionsfromarray', [$this, 'selectoptionsfromarray']),
+            new TwigFunction('selectOptions', [$this, 'selectOptions']),
             new TwigFunction('taxonomyoptions', [$this, 'taxonomyoptions']),
             new TwigFunction('taxonomyvalues', [$this, 'taxonomyvalues']),
             new TwigFunction('icon', [$this, 'icon'], $safe),
@@ -82,7 +93,21 @@ class RecordExtension extends AbstractExtension
         return "<i class='fas mr-2 fa-${icon}'></i>";
     }
 
-    public function selectoptionsfromarray(Field $field): LaravelCollection
+    public function selectOptions(Field $field): LaravelCollection
+    {
+        $values = $field->getDefinition()->get('values');
+
+
+        if (is_array($values)) {
+            return $this->selectOptionsArray($field);
+        } else {
+            return $this->selectOptionsContentType($field);
+        }
+
+
+    }
+
+    private function selectOptionsArray(Field $field): LaravelCollection
     {
         $values = $field->getDefinition()->get('values');
         $currentValues = $field->getValue();
@@ -111,6 +136,54 @@ class RecordExtension extends AbstractExtension
 
         return new LaravelCollection($options);
     }
+
+    private function selectOptionsContentType(Field $field): LaravelCollection
+    {
+        [ $contentTypeSlug, $fieldNames ] = explode('/', $field->getDefinition()->get('values'));
+
+        // @todo Actually do something with these, instead of using a default.
+        $fieldNames = explode(',', $fieldNames);
+
+        $currentValues = $field->getValue();
+
+        $options = [];
+
+        if ($field->getDefinition()->get('required', false)) {
+            $options[] = [
+                'key' => '',
+                'value' => '',
+                'selected' => false,
+            ];
+        }
+
+        $contentType = ContentType::factory($contentTypeSlug, $this->config->get('contenttypes'));
+
+        $maxAmount = $this->config->get('maximum_listing_select', 1000);
+
+        /** @var Content[] $records */
+        $records = $this->contentRepository->findForListing(1, $maxAmount, $contentType);
+
+        foreach ($records as $record) {
+            $options[] = [
+                'key' => $record->getId(),
+                'value' => sprintf('%s. %s (%s)', $record->getId(), $record->getExtras()['title'], $record->getStatus()),
+                'selected' => in_array($record->getId(), $currentValues, true),
+            ];
+        }
+
+        return new LaravelCollection($options);
+
+        foreach ($values as $key => $value) {
+            $options[] = [
+                'key' => $key,
+                'value' => $value,
+                'selected' => in_array($key, $currentValues, true),
+            ];
+        }
+
+        return new LaravelCollection($options);
+    }
+
 
     public function taxonomyoptions($taxonomy): LaravelCollection
     {
