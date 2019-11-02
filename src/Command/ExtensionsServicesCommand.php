ccf4b1e767502ce28520a8cf5e124f3c3a691321
Bob den Otter <bob@twokings.nl>
2019-11-02 07:11:14 +0100

    Add `console extensions:services` command

---
+++ src/Command/ExtensionsServicesCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Command;
+
+use Bolt\Extension\ExtensionRegistry;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ExtensionsServicesCommand extends Command
+{
+    protected static $defaultName = 'extensions:services';
+
+    /** @var ExtensionRegistry */
+    private $extensionRegistry;
+
+    /** @var ContainerInterface */
+    private $container;
+
+    public function __construct(ExtensionRegistry $extensionRegistry, ContainerInterface $container)
+    {
+        $this->extensionRegistry = $extensionRegistry;
+
+        parent::__construct();
+        $this->container = $container;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('List services available in Extensions');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): ?int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $rows = [];
+
+        $reflectedContainer = new \ReflectionClass($this->container);
+        $reflectionProperty = $reflectedContainer->getProperty('services');
+        $reflectionProperty->setAccessible(true);
+
+        $publicServices = $reflectionProperty->getValue($this->container);
+
+        foreach ($publicServices as $id => $name) {
+            $rows[] = [$id, get_class($name)];
+        }
+
+        $reflectionProperty = $reflectedContainer->getProperty('privates');
+        $reflectionProperty->setAccessible(true);
+
+        $privateServices = $reflectionProperty->getValue($this->container);
+
+        $io->text('Publicly accessible Services:');
+        $io->table(['Service ID', 'Class name / Alias'], $rows);
+
+        $rows = [];
+
+        foreach ($privateServices as $id => $name) {
+            $rows[] = [$id, get_class($name)];
+        }
+
+        $io->text('Private Services:');
+        $io->table(['Service ID', 'Class name / Alias'], $rows);
+
+        return 0;
+    }
+}
