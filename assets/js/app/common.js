9c4899b71e3c76fbe9961129d433f2ab330ca2de
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2019-11-04 16:31:43 +0100

    lint fixes

--- assets/js/app/common.js
+++ assets/js/app/common.js
@@ -26,26 +26,25 @@ $(document).ready(function() {
     }
   });
 
-
   /*
    ** Hash on tabs functionality
    ** When there is a Bootstrap data-toggle="pill" element, append the hash to the link
    */
-  let url = location.href.replace(/\/$/, "");
+  let url = location.href.replace(/\/$/, '');
   if (location.hash) {
-    const hash = url.split("#");
-    $('a[href="#' + hash[1] + '"]').tab("show");
-    url = location.href.replace(/\/#/, "#");
+    const hash = url.split('#');
+    $('a[href="#' + hash[1] + '"]').tab('show');
+    url = location.href.replace(/\/#/, '#');
     history.replaceState(null, null, url);
     setTimeout(() => {
       $(window).scrollTop(0);
     }, 50);
   }
 
-  $('a[data-toggle="pill"]').on("click", function() {
+  $('a[data-toggle="pill"]').on('click', function() {
     let newUrl;
-    const hash = $(this).attr("href");
-    newUrl = url.split("#")[0] + hash;
+    const hash = $(this).attr('href');
+    newUrl = url.split('#')[0] + hash;
     history.replaceState(null, null, newUrl);
   });
 });
