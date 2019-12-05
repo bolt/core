429dd30a5a1f87ad0c9b45359a08a3d44dd5a087
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2019-12-05 15:33:35 +0100

    cxfix and removing output

--- assets/js/app/editor/Components/Collection.vue
+++ assets/js/app/editor/Components/Collection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="collection-container" ref="collectionContainer">
+  <div ref="collectionContainer" class="collection-container">
     <div v-for="element in elements" :key="element.id" class="collection-item">
       <div :is="element"></div>
     </div>
@@ -49,7 +49,7 @@ export default {
         remove: '.action-remove-collection-item',
         moveUp: '.action-move-up-collection-item',
         moveDown: '.action-move-down-collection-item',
-      }
+      },
     };
   },
   computed: {
@@ -57,76 +57,97 @@ export default {
       return this.templateSelectOptions[0].key;
     },
   },
-  mounted(){
+  mounted() {
     let vueThis = this;
     /*
-   ** Event listeners on collection items buttons
-   ** This is a jQuery event listener, because Vue cannot handle an event emitted by a non-vue element.
-   ** The collection items are not Vue elements in order to initialise them correctly within their twig template.
-   */
+     ** Event listeners on collection items buttons
+     ** This is a jQuery event listener, because Vue cannot handle an event emitted by a non-vue element.
+     ** The collection items are not Vue elements in order to initialise them correctly within their twig template.
+     */
     $(document).on('click', vueThis.selector.remove, function(e) {
       e.preventDefault();
-      let collectionContainer = $(this).closest(vueThis.selector.collectionContainer);
+      let collectionContainer = $(this).closest(
+        vueThis.selector.collectionContainer,
+      );
       vueThis.getCollectionItemFromPressedButton(this).remove();
       vueThis.setAllButtonsStates(collectionContainer);
     });
 
-    $(document).on('click', vueThis.selector.moveUp, function(e){
+    $(document).on('click', vueThis.selector.moveUp, function(e) {
       e.preventDefault();
       let thisCollectionItem = vueThis.getCollectionItemFromPressedButton(this);
-      let prevCollectionitem = vueThis.getPreviousCollectionItem(thisCollectionItem);
+      let prevCollectionitem = vueThis.getPreviousCollectionItem(
+        thisCollectionItem,
+      );
       $(thisCollectionItem).after(prevCollectionitem);
 
       vueThis.setButtonsState(thisCollectionItem);
       vueThis.setButtonsState(prevCollectionitem);
     });
 
-    $(document).on('click', vueThis.selector.moveDown, function(e){
+    $(document).on('click', vueThis.selector.moveDown, function(e) {
       e.preventDefault();
       let thisCollectionItem = vueThis.getCollectionItemFromPressedButton(this);
-      let nextCollectionItem = vueThis.getNextCollectionItem(thisCollectionItem);
+      let nextCollectionItem = vueThis.getNextCollectionItem(
+        thisCollectionItem,
+      );
       $(thisCollectionItem).before(nextCollectionItem);
 
       vueThis.setButtonsState(thisCollectionItem);
       vueThis.setButtonsState(nextCollectionItem);
     });
   },
-  updated(){
-    console.log("updating");
+  updated() {
     this.setAllButtonsStates($(this.$refs.collectionContainer));
   },
   methods: {
-    setAllButtonsStates(collectionContainer){
+    setAllButtonsStates(collectionContainer) {
       let vueThis = this;
-      collectionContainer.children(vueThis.selector.item).each(function(){
-        console.log('iterating...');
-        console.log($(this));
+      collectionContainer.children(vueThis.selector.item).each(function() {
         vueThis.setButtonsState($(this));
       });
     },
-    setButtonsState(item){
+    setButtonsState(item) {
       //by default, enable
-      item.find(this.selector.moveUp).first().removeAttr('disabled');
-      item.find(this.selector.moveDown).first().removeAttr('disabled');
+      item
+        .find(this.selector.moveUp)
+        .first()
+        .removeAttr('disabled');
+      item
+        .find(this.selector.moveDown)
+        .first()
+        .removeAttr('disabled');
 
-      if(! this.getPreviousCollectionItem(item)){
+      if (!this.getPreviousCollectionItem(item)) {
         // first in collection
-        item.find(this.selector.moveUp).first().attr('disabled', 'disabled');
+        item
+          .find(this.selector.moveUp)
+          .first()
+          .attr('disabled', 'disabled');
       }
 
-      if (! this.getNextCollectionItem(item)){
+      if (!this.getNextCollectionItem(item)) {
         // last in collection
-        item.find(this.selector.moveDown).first().attr('disabled', 'disabled');
+        item
+          .find(this.selector.moveDown)
+          .first()
+          .attr('disabled', 'disabled');
       }
     },
-    getPreviousCollectionItem(item){
-      return item.prev('.collection-item').length === 0 ? false : item.prev('.collection-item');
+    getPreviousCollectionItem(item) {
+      return item.prev('.collection-item').length === 0
+        ? false
+        : item.prev('.collection-item');
     },
-    getNextCollectionItem(item){
-      return item.next('.collection-item').length === 0 ? false : item.next('.collection-item');
+    getNextCollectionItem(item) {
+      return item.next('.collection-item').length === 0
+        ? false
+        : item.next('.collection-item');
     },
-    getCollectionItemFromPressedButton(button){
-      return $(button).closest('.collection-item').last();
+    getCollectionItemFromPressedButton(button) {
+      return $(button)
+        .closest('.collection-item')
+        .last();
     },
     addCollectionItem() {
       let template = this.getSelectedTemplate();
