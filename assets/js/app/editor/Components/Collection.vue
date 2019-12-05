15263448eb7f654ca6f20f270c99c8daa365b261
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2019-12-05 15:30:32 +0100

    cleaning up. move up/down and remove buttons work

--- assets/js/app/editor/Components/Collection.vue
+++ assets/js/app/editor/Components/Collection.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div v-for="element in elements" :key="element.id">
+  <div class="collection-container" ref="collectionContainer">
+    <div v-for="element in elements" :key="element.id" class="collection-item">
       <div :is="element"></div>
     </div>
 
@@ -43,6 +43,13 @@ export default {
       elements: elements,
       templateSelectName: 'templateSelect' + this.id,
       templateSelectOptions: templateSelectOptions,
+      selector: {
+        collectionContainer: '.collection-container',
+        item: '.collection-item',
+        remove: '.action-remove-collection-item',
+        moveUp: '.action-move-up-collection-item',
+        moveDown: '.action-move-down-collection-item',
+      }
     };
   },
   computed: {
@@ -50,7 +57,77 @@ export default {
       return this.templateSelectOptions[0].key;
     },
   },
+  mounted(){
+    let vueThis = this;
+    /*
+   ** Event listeners on collection items buttons
+   ** This is a jQuery event listener, because Vue cannot handle an event emitted by a non-vue element.
+   ** The collection items are not Vue elements in order to initialise them correctly within their twig template.
+   */
+    $(document).on('click', vueThis.selector.remove, function(e) {
+      e.preventDefault();
+      let collectionContainer = $(this).closest(vueThis.selector.collectionContainer);
+      vueThis.getCollectionItemFromPressedButton(this).remove();
+      vueThis.setAllButtonsStates(collectionContainer);
+    });
+
+    $(document).on('click', vueThis.selector.moveUp, function(e){
+      e.preventDefault();
+      let thisCollectionItem = vueThis.getCollectionItemFromPressedButton(this);
+      let prevCollectionitem = vueThis.getPreviousCollectionItem(thisCollectionItem);
+      $(thisCollectionItem).after(prevCollectionitem);
+
+      vueThis.setButtonsState(thisCollectionItem);
+      vueThis.setButtonsState(prevCollectionitem);
+    });
+
+    $(document).on('click', vueThis.selector.moveDown, function(e){
+      e.preventDefault();
+      let thisCollectionItem = vueThis.getCollectionItemFromPressedButton(this);
+      let nextCollectionItem = vueThis.getNextCollectionItem(thisCollectionItem);
+      $(thisCollectionItem).before(nextCollectionItem);
+
+      vueThis.setButtonsState(thisCollectionItem);
+      vueThis.setButtonsState(nextCollectionItem);
+    });
+  },
+  updated(){
+    console.log("updating");
+    this.setAllButtonsStates($(this.$refs.collectionContainer));
+  },
   methods: {
+    setAllButtonsStates(collectionContainer){
+      let vueThis = this;
+      collectionContainer.children(vueThis.selector.item).each(function(){
+        console.log('iterating...');
+        console.log($(this));
+        vueThis.setButtonsState($(this));
+      });
+    },
+    setButtonsState(item){
+      //by default, enable
+      item.find(this.selector.moveUp).first().removeAttr('disabled');
+      item.find(this.selector.moveDown).first().removeAttr('disabled');
+
+      if(! this.getPreviousCollectionItem(item)){
+        // first in collection
+        item.find(this.selector.moveUp).first().attr('disabled', 'disabled');
+      }
+
+      if (! this.getNextCollectionItem(item)){
+        // last in collection
+        item.find(this.selector.moveDown).first().attr('disabled', 'disabled');
+      }
+    },
+    getPreviousCollectionItem(item){
+      return item.prev('.collection-item').length === 0 ? false : item.prev('.collection-item');
+    },
+    getNextCollectionItem(item){
+      return item.next('.collection-item').length === 0 ? false : item.next('.collection-item');
+    },
+    getCollectionItemFromPressedButton(button){
+      return $(button).closest('.collection-item').last();
+    },
     addCollectionItem() {
       let template = this.getSelectedTemplate();
 
