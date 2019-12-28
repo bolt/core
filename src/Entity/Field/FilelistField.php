5839a0337243eef41b3c78f8b2078aef79bc9866
Bob den Otter <bob@twokings.nl>
2019-12-28 15:42:42 +0100

    Make Filelist work in frontend

--- src/Entity/Field/FilelistField.php
+++ src/Entity/Field/FilelistField.php
@@ -18,16 +18,41 @@ class FilelistField extends Field implements FieldInterface
         return 'filelist';
     }
 
+    /**
+     * Returns the value, as is in the database. Useful for processing, like
+     * editing in the backend, where the results are to be serialised
+     */
+    public function getRawValue(): array
+    {
+        return (array) parent::getValue() ?: [];
+    }
+
+    /**
+     * Returns the value, where the contained fields are "hydrated" as actual
+     * File Fields. For example, for iterating in the frontend
+     */
     public function getValue(): array
     {
-        $files = (array) parent::getValue() ?: [];
         $result = [];
-        foreach ($files as $key => $file) {
+
+        foreach ($this->getRawValue() as $key => $file) {
             $fileField = new FileField();
             $fileField->setName((string) $key);
             $fileField->setValue($file);
-            array_push($result, $fileField->getValue());
+            array_push($result, $fileField);
         }
+
         return $result;
+    }
+
+    /**
+     * Returns the value, where the contained Image fields are seperately
+     * casted to arrays, including the "extras"
+     */
+    public function getJsonValue()
+    {
+        return json_encode(array_map(function (FileField $i) {
+            return $i->getValue();
+        }, $this->getValue()));
     }
 }
