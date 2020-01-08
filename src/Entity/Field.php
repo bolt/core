0ec455f2d2d77aedace5577e4b4a7f09ad228612
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-01-08 13:07:09 +0100

    use getParsedValue! :)

--- src/Entity/Field.php
+++ src/Entity/Field.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
 use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Tightenco\Collect\Support\Collection as LaravelCollection;
 use Twig\Markup;
 
@@ -165,10 +166,6 @@ class Field implements FieldInterface, TranslatableInterface
         return $this->translate($this->getCurrentLocale())->get($key);
     }
 
-    /**
-     * @return array|null
-     * @Groups("get_field")
-     */
     public function getValue(): ?array
     {
         return $this->translate($this->getCurrentLocale())->getValue();
@@ -178,6 +175,8 @@ class Field implements FieldInterface, TranslatableInterface
      * like getValue() but returns single value for single value fields
      *
      * @return array|mixed|null
+     * @Groups("get_field")
+     * @SerializedName("value")
      */
     public function getParsedValue()
     {
