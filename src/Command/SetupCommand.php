707043a9f3e81805017475834123a128e40fa8ce
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-01-20 15:18:53 +0100

    Add --no-fixtures flag to setup command

--- src/Command/SetupCommand.php
+++ src/Command/SetupCommand.php
@@ -7,6 +7,7 @@ namespace Bolt\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -16,7 +17,10 @@ class SetupCommand extends Command
 
     protected function configure(): void
     {
-        $this->setDescription('Run Bolt setup / installation commands');
+        $this
+            ->setDescription('Run Bolt setup / installation commands')
+            ->addOption('no-fixtures', null, InputOption::VALUE_NONE, 'If set, no data fixtures will be created. An empty database wil be initialised.')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -36,9 +40,12 @@ class SetupCommand extends Command
         $commandInput = new ArrayInput(['--admin' => true]);
         $exitCode += $command->run($commandInput, $output);
 
-        $command = $this->getApplication()->find('doctrine:fixtures:load');
-        $commandInput = new ArrayInput(['--append' => true]);
-        $exitCode += $command->run($commandInput, $output);
+        $noFixtures = $input->getOption('no-fixtures');
+        if(! $noFixtures) {
+            $command = $this->getApplication()->find('doctrine:fixtures:load');
+            $commandInput = new ArrayInput(['--append' => true]);
+            $exitCode += $command->run($commandInput, $output);
+        }
 
         $io->newLine();
 
