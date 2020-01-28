231389d3f9a1e50c9423099f5ecd00943591ff27
Bob den Otter <bob@twokings.nl>
2020-01-28 18:23:04 +0100

    Implement `{{ dump(backtrace() }}`

---
+++ src/Twig/DebugExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class DebugExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('backtrace', [$this, 'backtrace']),
+        ];
+    }
+
+    public function backtrace(int $options = DEBUG_BACKTRACE_IGNORE_ARGS, int $limit = 25): array
+    {
+        return debug_backtrace($options, $limit);
+    }
+}
