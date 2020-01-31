101600ab71c8423911289254710904a55e45b304
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-01-31 14:17:45 +0100

    Show invalid field form another group

--- assets/js/app/common.js
+++ assets/js/app/common.js
@@ -60,4 +60,21 @@ $(document).ready(function() {
    ** Initialise all popover elements
    */
   $('[data-toggle="popover"]').popover();
+
+  /*
+   ** When a field from another group is invalid, show it.
+   */
+  $('#editor button[type="submit"]').click(function () {
+    $('input:invalid').each(function () {
+      // Find the tab-pane that this element is inside, and get the id
+      var $closest = $(this).closest('.tab-pane');
+      var id = $closest.attr('id');
+
+      // Find the link that corresponds to the pane and have it show
+      $('.nav a[href="#' + id + '"]').tab('show');
+
+      // Only want to do it once
+      return false;
+    });
+  });
 });
