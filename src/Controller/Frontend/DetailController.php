9aeb33631988fb54f3f19bd38965cf0b8c60af35
Bob den Otter <bob@twokings.nl>
2020-02-01 16:08:38 +0100

    Update DetailController.php

--- src/Controller/Frontend/DetailController.php
+++ src/Controller/Frontend/DetailController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bolt\Controller\Frontend;
 
-use Bolt\Configuration\Config;
 use Bolt\Configuration\Content\ContentType;
 use Bolt\Controller\TwigAwareController;
 use Bolt\Enum\Statuses;
