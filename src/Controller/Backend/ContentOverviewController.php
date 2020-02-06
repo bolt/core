6774fbbda46b75d6a1caff722426b945b00181de
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-02-06 14:24:35 +0100

    Content listing uses Query

--- src/Controller/Backend/ContentOverviewController.php
+++ src/Controller/Backend/ContentOverviewController.php
@@ -6,7 +6,9 @@ namespace Bolt\Controller\Backend;
 
 use Bolt\Configuration\Content\ContentType;
 use Bolt\Controller\TwigAwareController;
+use Bolt\Storage\Query;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,12 +20,38 @@ class ContentOverviewController extends TwigAwareController implements BackendZo
     /**
      * @Route("/content/{contentType}", name="bolt_content_overview")
      */
-    public function overview(string $contentType = ''): Response
+    public function overview(Request $request, Query $query, string $contentType = ''): Response
     {
-        $contentType = ContentType::factory($contentType, $this->config->get('contenttypes'));
+        $contentTypeObject = ContentType::factory($contentType, $this->config->get('contenttypes'));
+
+        $params = [
+            'status' => '!unknown'
+        ];
+
+        if($request->get('sortBy'))
+        {
+            $params['order'] = $request->get('sortBy');
+        }
+
+        if($request->get('filter'))
+        {
+            $params['anyField'] = '%' . $request->get('filter') . '%';
+        }
+
+        if($request->get('taxonomy'))
+        {
+            $taxonomy = explode('=', $request->get('taxonomy'));
+            $params[$taxonomy[0]] = $taxonomy[1];
+        }
+
+        $records = $query->getContentForTwig($contentType, $params)
+            ->setMaxPerPage($contentTypeObject->get('records_per_page'));
 
         return $this->renderTemplate('@bolt/content/listing.html.twig', [
-            'contentType' => $contentType,
+            'contentType' => $contentTypeObject,
+            'records' => $records,
+            'sortBy' => $request->get('sortBy'),
+            'filterValue' => $request->get('filter'),
         ]);
     }
 }
