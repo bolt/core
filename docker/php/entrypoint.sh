ab92f16f551e784b514b5083c57af1459cf19075
Flug <flugv1@gmail.com>
2020-02-17 22:09:57 +0100

    dockefiles layer division
    division of the dev layer from the potential production layer.
    Changing the entrypoint for the dev part that installs the dependencies.
    Change of the nginx part. Adding an argument to the build for
    the different PDO drivers (mysql , postgresql etc... )

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
+set -e
 
-export CONFD_ARGS="${CONFD_ARGS:--onetime -backend env}"
-export DOCKER_BRIDGE_IP=$(ip ro | grep default | cut -d' ' -f 3)
-if [ "$1" = 'php-fpm' ]; then
-    /bin/confd ${CONFD_ARGS}
-    php-fpm
-else
-    /bin/confd ${CONFD_ARGS}
-    exec /usr/bin/env "$@"
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
 fi
+
+if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+	if [ "$APP_ENV" != 'prod' ]; then
+		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+	fi
+	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+
+	mkdir -p var/cache var/log
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+
+	if [ "$APP_ENV" != 'prod' ]; then
+		composer install --prefer-dist --no-progress --no-suggest --no-interaction
+	fi
+fi
+
+exec docker-php-entrypoint "$@"
+
