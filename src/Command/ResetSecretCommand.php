b161c589f6f7e398d2ba287440df214b4ed34f0d
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-02-21 13:55:12 +0100

    Create bolt:replace-secret command to generate new APP_SECRET

---
+++ src/Command/ResetSecretCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Webimpress\SafeWriter\Exception\ExceptionInterface;
+use Webimpress\SafeWriter\FileWriter;
+
+class ResetSecretCommand extends Command
+{
+    protected static $defaultName = 'bolt:reset-secret';
+
+    /** @var string */
+    private $projectDir;
+
+    public function __construct(string $projectDir)
+    {
+        parent::__construct();
+        $this->projectDir = $projectDir;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Reset the APP_SECRET for this Bolt site.')
+            ->setHelp(
+                <<<'HELP'
+The <info>%command.name%</info> command generates a new APP_SECRET in the .env file
+HELP
+            );
+    }
+
+    /**
+     * This method is executed after initialize(). It usually contains the logic
+     * to execute to complete this command task.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $filepath = $this->projectDir . '/.env.dist';
+        $content = file_get_contents($filepath);
+        $newSecret = 'APP_SECRET=' . bin2hex(random_bytes(16));
+        $matchSecret = '/APP_SECRET=(.*)/';
+
+        $newContent = preg_replace($matchSecret, $newSecret, $content);
+
+        try {
+            FileWriter::writeFIle($filepath, $newContent);
+        } catch (ExceptionInterface $e) {
+            $message = sprintf('Failed to replace APP_SECRET. %s', $e->getMessage());
+            $io->error($message);
+        }
+
+        $io->success('Secret replaced successfully.');
+        return null;
+    }
+}
