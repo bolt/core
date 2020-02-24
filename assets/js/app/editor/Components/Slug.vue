f878f0ca0b8c45ddbdd3217639523fa50f024e41
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-02-24 10:15:59 +0100

    Don't reload page on Slug events (edit, lock, generate)

--- assets/js/app/editor/Components/Slug.vue
+++ assets/js/app/editor/Components/Slug.vue
@@ -30,16 +30,16 @@
         </button>
         <div class="dropdown-menu">
           <template v-if="!edit">
-            <button class="dropdown-item" @click="editSlug">
+            <button class="dropdown-item" @click="editSlug" type="button">
               <i class="fas fa-pencil-alt fa-fw"></i> {{ labels.button_edit }}
             </button>
           </template>
           <template v-if="!locked">
-            <button class="dropdown-item" @click="lockSlug">
+            <button class="dropdown-item" @click="lockSlug" type="button">
               <i class="fas fa-lock fa-fw"></i> {{ labels.button_locked }}
             </button>
           </template>
-          <button class="dropdown-item" @click="generateSlug()">
+          <button class="dropdown-item" @click="generateSlug" type="button">
             <i class="fas fa-link fa-fw"></i> {{ labels.generate_from }}
             {{ generate }}
           </button>
