2fe98ac1f8252d1f8a0e1c358429b87022de1624
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-03-13 15:21:53 +0100

    csfix

--- src/Entity/Relation.php
+++ src/Entity/Relation.php
@@ -6,9 +6,9 @@ namespace Bolt\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
-use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ApiResource(
