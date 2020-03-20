ee0d73cd73d8e7a0a0749817a8c165a68dab00e0
Bob den Otter <bob@twokings.nl>
2020-03-20 07:25:08 +0100

    Update Version.php

--- src/Version.php
+++ src/Version.php
@@ -21,7 +21,7 @@ final class Version
      *   Stable      — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.0.0 RC 10';
+    public const VERSION = '4.0.0 RC 11';
     public const CODENAME = 'Getting closer to a stable release (just a few more RC\'s to iron out the kinks)';
 
     /**
