bd9e5430ddedf18869fd5311f36161a583ae2ee9
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-05-18 14:16:06 +0200

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -56,8 +56,6 @@ class Kernel extends BaseKernel
         $container->setParameter('container.dumper.inline_factories', true);
         $confDir = $this->getProjectDir() . '/config';
 
-        $this->setBoltParameters($container, $confDir);
-        $this->setContentTypeRequirements($container);
         $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
         $loader->load($confDir . '/{packages}/' . $this->environment . '/*' . self::CONFIG_EXTS, 'glob');
         $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
