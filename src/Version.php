6514e93b3be6cb5293c33fdb37dfa900c73fe289
Bob den Otter <bob@twokings.nl>
2020-06-18 13:55:46 +0200

    Prepare release 4.0.0-RC.25

--- src/Version.php
+++ src/Version.php
@@ -21,7 +21,7 @@ final class Version
      *   Stable      — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.0.0 RC 24';
+    public const VERSION = '4.0.0 RC 25';
     public const CODENAME = 'Really? This many RC\'s? Yes, really!';
 
     /**
