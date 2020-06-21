71503206f64c176964e132459ac6440b3b35ae3a
Bob den Otter <bob@twokings.nl>
2020-06-21 12:41:26 +0200

    Ensure $boltConfig is set for Controllers in extensions

--- src/Extension/ExtensionController.php
+++ src/Extension/ExtensionController.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Bolt\Extension;
 
+use Bolt\Configuration\Config;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ExtensionController extends AbstractController
 {
+    public function __construct(Config $config)
+    {
+        $this->boltConfig = $config;
+    }
+
     use ServicesTrait;
     use ConfigTrait;
 }
