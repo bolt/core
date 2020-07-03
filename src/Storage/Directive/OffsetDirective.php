f0e1a9d63e32a1d303ccf5b4e35757b6e4f99942
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-07-03 16:28:31 +0200

    pass those tests

--- src/Storage/Directive/OffsetDirective.php
+++ src/Storage/Directive/OffsetDirective.php
@@ -11,7 +11,7 @@ use Bolt\Storage\SelectQuery;
  */
 class OffsetDirective
 {
-    public const NAME = 'page';
+    public const NAME = 'paging';
 
     public function __invoke(SelectQuery $query, int $page, array $otherDirectives): void
     {
