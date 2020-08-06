5699b0a57e13d0004cef5d936266d386c0f04076
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-08-06 15:30:02 +0200

    Allow textarea `height` option

--- assets/js/app/editor/Components/Textarea.vue
+++ assets/js/app/editor/Components/Textarea.vue
@@ -4,12 +4,13 @@
       v-model="rawVal"
       class="form-control field--textarea"
       :name="name"
-      rows="10"
+      :rows="rows"
       :required="required"
       :readonly="readonly"
       :data-errormessage="errormessage"
       :pattern="pattern"
       :placeholder="placeholder"
+      :style="{ height: styleHeight }"
     ></textarea>
   </div>
 </template>
@@ -28,6 +29,15 @@ export default {
     errormessage: String | Boolean,
     pattern: String | Boolean,
     placeholder: String | Boolean,
+    height: String | Number,
+  },
+  data() {
+    return {
+      // If height is 50px/vh/vw/%, set css height.
+      // If height is 50 (a number), set rows attribute.
+      rows: isNaN(this.height) ? false : this.height,
+      styleHeight: isNaN(this.height) ? this.height : false,
+    };
   },
 };
 </script>
