a5bb2401d6c73004cf7547699e622f4e6f449abe
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-08-10 12:27:33 +0200

    csfix

--- src/Event/Listener/FieldFillListener.php
+++ src/Event/Listener/FieldFillListener.php
@@ -7,13 +7,13 @@ namespace Bolt\Event\Listener;
 use Bolt\Configuration\Content\FieldType;
 use Bolt\Entity\Field;
 use Bolt\Entity\Field\CollectionField;
+use Bolt\Entity\Field\RawPersistable;
 use Bolt\Entity\Field\SetField;
 use Bolt\Entity\FieldInterface;
 use Bolt\Entity\FieldTranslation;
 use Bolt\Repository\FieldRepository;
 use Bolt\Utils\Sanitiser;
 use Doctrine\ORM\Event\LifecycleEventArgs;
-use Bolt\Entity\Field\RawPersistable;
 
 class FieldFillListener
 {
@@ -36,7 +36,8 @@ class FieldFillListener
     public function preUpdate(LifecycleEventArgs $args): void
     {
         $entity = $args->getEntity();
-        if($entity instanceof FieldTranslation && $entity->getTranslatable() instanceof FieldInterface) {
+        if ($entity instanceof FieldTranslation && $entity->getTranslatable() instanceof FieldInterface) {
+            /** @var Field $field */
             $field = $entity->getTranslatable();
 
             if (! $field instanceof RawPersistable) {
