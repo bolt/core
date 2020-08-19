e3ca60ba148b24ae123257ef19ef0976bbd258e4
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-08-19 16:21:51 +0200

    Current filter works for locale

--- src/Twig/CommonExtension.php
+++ src/Twig/CommonExtension.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Bolt\Twig;
 
 use Bolt\Entity\Content;
+use Tightenco\Collect\Support\Collection;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
@@ -16,10 +17,14 @@ class CommonExtension extends AbstractExtension
     /** @var FrontendMenuExtension */
     private $frontendMenuExtension;
 
-    public function __construct(ContentExtension $contentExtension, FrontendMenuExtension $frontendMenuExtension)
+    /** @var LocaleExtension */
+    private $localeExtension;
+
+    public function __construct(ContentExtension $contentExtension, FrontendMenuExtension $frontendMenuExtension, LocaleExtension $localeExtension)
     {
         $this->contentExtension = $contentExtension;
         $this->frontendMenuExtension = $frontendMenuExtension;
+        $this->localeExtension = $localeExtension;
     }
 
     /**
@@ -40,8 +45,24 @@ class CommonExtension extends AbstractExtension
             return $this->contentExtension->isCurrent($env, $item);
         } elseif (is_iterable($item) && array_key_exists('uri', $item)) {
             return $this->frontendMenuExtension->isCurrent($item);
+        } elseif ($this->getLocale($item)) {
+            return $this->localeExtension->getHtmlLang($env) === $this->getLocale($item);
         }
 
         return false;
+    }
+
+    private function getLocale($item): ?string
+    {
+        if (is_string($item)) {
+            $localepattern = '/^[a-z]{2}((-|_)[a-z]{2})?$/m';
+            preg_match_all($localepattern, $item, $matches);
+
+            return ! empty($matches) ? $item : null;
+        } elseif ($item instanceof Collection) {
+            return $this->getLocale($item->get('code', null));
+        }
+
+        return null;
     }
 }
