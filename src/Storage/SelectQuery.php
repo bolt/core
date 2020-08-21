582c4a216a86a8b9fe035af019d7f9a7f859cdb3
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-08-21 15:44:04 +0200

    csfix

--- src/Storage/SelectQuery.php
+++ src/Storage/SelectQuery.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bolt\Storage;
 
-use Bolt\Common\Json;
 use Bolt\Configuration\Config;
 use Bolt\Configuration\Content\ContentType;
 use Bolt\Doctrine\JsonHelper;
