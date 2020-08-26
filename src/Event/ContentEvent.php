ffd8a779e316bad2b56b1a22753219c37b64efa6
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-08-26 10:06:23 +0200

    More content events

--- src/Event/ContentEvent.php
+++ src/Event/ContentEvent.php
@@ -15,6 +15,9 @@ class ContentEvent extends Event
     public const ON_PREVIEW = 'bolt.pre_edit';
     public const ON_DUPLICATE = 'bolt.on_duplicate';
     public const PRE_STATUS_CHANGE = 'bolt.pre_status_change';
+    public const POST_STATUS_CHANGE = 'bolt.post_status_change';
+    public const PRE_DELETE = 'bolt.pre_delete';
+    public const POST_DELETE = 'bolt.post_delete';
 
     /** @var Content */
     private $content;
