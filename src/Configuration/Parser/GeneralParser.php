c198a0164c897236082e96141e257b07e2ab4b1a
Bob den Otter <bob@twokings.nl>
2020-09-16 15:58:35 +0200

    Update GeneralParser.php

--- src/Configuration/Parser/GeneralParser.php
+++ src/Configuration/Parser/GeneralParser.php
@@ -83,6 +83,9 @@ class GeneralParser extends BaseParser
             'accept_upload_size' => '8M',
             'upload_location' => '{contenttype}/{year}/{month}/',
             'maintenance_mode' => false,
+            'headers' => [
+                'x_frame_options' => true,
+            ],
             'htmlcleaner' => [
                 'allowed_tags' => explode(',', 'div,span,p,br,hr,s,u,strong,em,i,b,li,ul,ol,mark,blockquote,pre,code,tt,h1,h2,h3,h4,h5,h6,dd,dl,dh,table,tbody,thead,tfoot,th,td,tr,a,img,address,abbr,iframe'),
                 'allowed_attributes' => explode(',', 'id,class,style,name,value,href,Bolt,alt,title,width,height,frameborder,allowfullscreen,scrolling'),
