9d4a75525c85df4aab09ac29e7b26f3838d5285d
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2020-10-12 20:03:04 +0200

    changed all generated app_ routes to bolt_ routes and other issues pointed out by bob

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -42,7 +42,7 @@ class ResetPasswordController extends AbstractController
     /**
      * Display & process form to request a password reset.
      *
-     * @Route("", name="app_forgot_password_request")
+     * @Route("", name="bolt_forgot_password_request")
      */
     public function request(Request $request, MailerInterface $mailer): Response
     {
@@ -64,13 +64,13 @@ class ResetPasswordController extends AbstractController
     /**
      * Confirmation page after a user has requested a password reset.
      *
-     * @Route("/check-email", name="app_check_email")
+     * @Route("/check-email", name="bolt_check_email")
      */
     public function checkEmail(): Response
     {
         // We prevent users from directly accessing this page
         if (! $this->canCheckEmail()) {
-            return $this->redirectToRoute('app_forgot_password_request');
+            return $this->redirectToRoute('bolt_forgot_password_request');
         }
 
         return $this->render('reset_password/check_email.html.twig', [
@@ -81,7 +81,7 @@ class ResetPasswordController extends AbstractController
     /**
      * Validates and process the reset URL that the user clicked in their email.
      *
-     * @Route("/reset/{token}", name="app_reset_password")
+     * @Route("/reset/{token}", name="bolt_reset_password")
      */
     public function reset(Request $request, UserPasswordEncoderInterface $passwordEncoder, ?string $token = null): Response
     {
@@ -90,7 +90,7 @@ class ResetPasswordController extends AbstractController
             // loaded in a browser and potentially leaking the token to 3rd party JavaScript.
             $this->storeTokenInSession($token);
 
-            return $this->redirectToRoute('app_reset_password');
+            return $this->redirectToRoute('bolt_reset_password');
         }
 
         $token = $this->getTokenFromSession();
@@ -106,7 +106,7 @@ class ResetPasswordController extends AbstractController
                 $e->getReason()
             ));
 
-            return $this->redirectToRoute('app_forgot_password_request');
+            return $this->redirectToRoute('bolt_forgot_password_request');
         }
 
         // The token is valid; allow the user to change their password.
@@ -146,19 +146,19 @@ class ResetPasswordController extends AbstractController
             'email' => $emailFormData,
         ]);
 
-        // Marks that you are allowed to see the app_check_email page.
+        // Marks that you are allowed to see the bolt_check_email page.
         $this->setCanCheckEmailInSession();
 
         // Do not reveal whether a user account was found or not.
         if (! $user) {
-            return $this->redirectToRoute('app_check_email');
+            return $this->redirectToRoute('bolt_check_email');
         }
 
         try {
             $resetToken = $this->resetPasswordHelper->generateResetToken($user);
         } catch (ResetPasswordExceptionInterface $e) {
             // If you want to tell the user why a reset email was not sent, uncomment
-            // the lines below and change the redirect to 'app_forgot_password_request'.
+            // the lines below and change the redirect to 'bolt_forgot_password_request'.
             // Caution: This may reveal if a user is registered or not.
             //
             // $this->addFlash('reset_password_error', sprintf(
@@ -166,7 +166,7 @@ class ResetPasswordController extends AbstractController
             //     $e->getReason()
             // ));
 
-            return $this->redirectToRoute('app_check_email');
+            return $this->redirectToRoute('bolt_check_email');
         }
 
         // Global config/bolt/config.yml file.
@@ -184,6 +184,6 @@ class ResetPasswordController extends AbstractController
 
         $mailer->send($email);
 
-        return $this->redirectToRoute('app_check_email');
+        return $this->redirectToRoute('bolt_check_email');
     }
 }
