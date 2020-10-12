5da285ce3231a1d510a09899e5bd10a0251f0304
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2020-10-12 20:03:04 +0200

    changed all generated app_ routes to bolt_ routes and other issues pointed out by bob

--- src/.preload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (file_exists(__DIR__.'/../var/cache/dev/Bolt_KernelDevDebugContainer.preload.php')) {
-    require __DIR__.'/../var/cache/dev/Bolt_KernelDevDebugContainer.preload.php';
-}
