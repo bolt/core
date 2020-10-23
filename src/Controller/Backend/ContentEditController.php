01679209bc928a328675b3aae9bfe42d67f9b611
Simon Groenewolt <simon@ijsfontein.nl>
2020-10-23 13:02:45 +0200

    add 'flash' to indicate error saving

--- src/Controller/Backend/ContentEditController.php
+++ src/Controller/Backend/ContentEditController.php
@@ -138,6 +138,7 @@ class ContentEditController extends TwigAwareController implements BackendZoneIn
         if ($contentValidator) {
             $constraintViolations = $contentValidator->validate($content);
             if (count($constraintViolations) > 0) {
+                $this->addFlash('danger', 'content.validation_errors');
                 return $this->renderEditor($content, $constraintViolations);
             }
         }
