ff1e539aadd5405f61b05a35cdf720d081b05c21
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-10-23 10:11:26 +0200

    Use remember me field in login Symfony Form

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -6,6 +6,7 @@ namespace Bolt\Form;
 
     use Cocur\Slugify\Slugify;
     use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
     use Symfony\Component\Form\Extension\Core\Type\PasswordType;
     use Symfony\Component\Form\Extension\Core\Type\TextType;
     use Symfony\Component\Form\FormBuilderInterface;
@@ -58,6 +59,8 @@ namespace Bolt\Form;
                     'attr' => [
                         'placeholder' => 'placeholder.password',
                     ],
+                ])->add('remember_me', CheckboxType::class, [
+                    'label' => 'label.rememberme',
                 ]);
         }
 
