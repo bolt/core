2f61a7be0f6424e0c8610eac3e3917ee6c2951a8
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-11-17 14:30:41 +0100

    `|selected` filter orders by the value of the select

--- src/Twig/FieldExtension.php
+++ src/Twig/FieldExtension.php
@@ -105,7 +105,13 @@ class FieldExtension extends AbstractExtension
 
         $ids = $field->getValue();
         // Find records by their respective ids
-        $records = $this->contentRepository->findBy(['id' => $ids]);
+        $records = collect($this->contentRepository->findBy(['id' => $ids]));
+
+        // Sort the results in the order of the $ids.
+        $order = array_flip($ids);
+        $records = $records->sortBy(function (Content $record) use ($order) {
+            return $order[$record->getId()];
+        })->toArray();
 
         if ($returnsingle || (! $returnarray && $definition->get('multiple') === false)) {
             return current($records);
