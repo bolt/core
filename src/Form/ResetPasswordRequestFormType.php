a6c9fd33358300dff97af3dc8330dd5b497c9eff
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2020-11-17 18:42:38 +0100

    fix reset password logic not being translatable

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -9,16 +9,25 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ResetPasswordRequestFormType extends AbstractType
 {
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email', EmailType::class, [
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter your email',
+                        'message' => $this->translator->trans('form.empty_email'),
                     ]),
                 ],
             ])
