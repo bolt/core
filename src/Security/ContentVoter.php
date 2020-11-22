fae7dab811d1f94ebd00e66a6fcaa767500a4cea
Simon Groenewolt <simon@ijsfontein.nl>
2020-11-23 00:56:19 +0100

    ContentVoter.php first stab at actually implementing the rules with all, default, and specific contenttype settings.

--- src/Security/ContentVoter.php
+++ src/Security/ContentVoter.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace Bolt\Security;
 
 use Bolt\Configuration\Config;
+use Bolt\Configuration\Content\ContentType;
 use Bolt\Entity\Content;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Tightenco\Collect\Support\Collection;
 
 class ContentVoter extends Voter
@@ -28,90 +29,126 @@ class ContentVoter extends Voter
 #                     permission can indirectly enable users more permissions
 #                     in ways that may not be immediately obvious.
      */
-    public const CONTENT_EDIT = 'content_edit';
-    public const CONTENT_CREATE = 'content_create';
-    public const CONTENT_PUBLISH = 'content_publish';
-    public const CONTENT_DEPUBLISH = 'content_depublish';
-    public const CONTENT_DELETE = 'content_delete';
-    public const CONTENT_CHANGE_OWNERSHIP = 'content_change-ownership';
+    public const CONTENT_EDIT = 'edit';
+    public const CONTENT_CREATE = 'create';
+    public const CONTENT_PUBLISH = 'publish';
+    public const CONTENT_DEPUBLISH = 'depublish';
+    public const CONTENT_DELETE = 'delete';
+    public const CONTENT_CHANGE_OWNERSHIP = 'change-ownership';
+    public const CONTENT_VIEW = 'view';
 
     private $security;
     private $supportedAttributes;
+
+    /** @var Collection|null */
+    private $contenttypePermissionsAll;
+    /** @var Collection|null */
+    private $contenttypePermissionsDefault;
+    /** @var Collection|null */
+    private $contenttypePermissions;
 
     public function __construct(Security $security, Config $config)
     {
         $this->security = $security;
 
-        $config->get('permissions/contenttype-all');
-        $config->get('permissions/contenttype-default');
-        $config->get('permissions/contenttypes');
+        $this->contenttypePermissionsAll = $config->get('permissions/contenttype-all', collect([]));
+        $this->contenttypePermissionsDefault = $config->get('permissions/contenttype-default', collect([]));
+        $this->contenttypePermissions = $config->get('permissions/contenttypes', null);
 
-        if ($this->globalPermissions instanceof Collection) {
-            // TODO should we also validate that the values are all simple arrays?
-            $globalPermissionNames = array_keys($this->globalPermissions->all());
-            foreach ($globalPermissionNames as $attribute) {
-                $this->supportedAttributes[] = $attribute;
-            }
-        } else {
-            throw new \DomainException('No global permissions config found');
+        if (! ($this->contenttypePermissionsAll instanceof Collection)) {
+            throw new \DomainException('No permissions config found');
+        }
+        if (! ($this->contenttypePermissionsDefault instanceof Collection)) {
+            throw new \DomainException('No permissions config found');
+        }
+        if (! ($this->contenttypePermissions == null || $this->contenttypePermissions instanceof Collection)) {
+            throw new \DomainException('No permissions config found');
         }
     }
 
     protected function supports(string $attribute, $subject)
     {
+        // only vote on `Content` and `ContentType` objects
+        if (! ($subject instanceof Content || $subject instanceof ContentType)) {
+            return false;
+        }
+
         // if the attribute isn't one we support, return false
-        if (! in_array($attribute, [self::CONTENT_EDIT, self::CONTENT_CREATE, self::CONTENT_PUBLISH,
+        return in_array($attribute, [self::CONTENT_EDIT, self::CONTENT_CREATE, self::CONTENT_PUBLISH,
             self::CONTENT_DEPUBLISH, self::CONTENT_DELETE, self::CONTENT_CHANGE_OWNERSHIP,
-            self::CONTENT_VIEW, ], true)) {
-            return false;
-        }
-        return in_array($attribute, $this->supportedAttributes, true);
-
-        // only vote on `Content` objects
-        if (! $subject instanceof Content) {
-            return false;
-        }
-
-        return true;
+            self::CONTENT_VIEW], true);
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token)
     {
-        if ($this->security->isGranted('ROLE_ADMIN')) {
-            return true;
-        }
-
         $user = $token->getUser();
 
-        if (! $user instanceof User) {
+        if (! $user instanceof UserInterface) {
             // the user must be logged in; if not, deny access
             return false;
         }
 
-        // you know $subject is a Post object, thanks to `supports()`
-        /** @var Content $content */
-        $content = $subject;
-
-        switch ($attribute) {
-            case self::CONTENT_EDIT:
-                return $this->canEdit($content, $user);
+        // first check if the users has an 'all' permission set for this content(type)
+        $allRoles = $this->contenttypePermissionsAll->get($attribute);
+        if ($allRoles && $allRoles instanceof Collection) {
+            // check if user is granted any of the specified attributes/roles
+            foreach ($allRoles as $role) {
+                if ($this->security->isGranted($role, $subject)) {
+                    return true;
+                }
+            }
         }
 
-        throw new \LogicException('This code should not be reached!');
-    }
-
-    private function canView(Content $content, User $user)
-    {
-        // if they can edit, they can view
-        if ($this->canEdit($content, $user)) {
-            return true;
+        $contentTypeName = null;
+        if ($subject instanceof Content) {
+            /** @var Content $content */
+            $content = $subject;
+            $contentTypeName = $content->getContentType();
+        } else if ($subject instanceof ContentType) {
+            /** @var ContentType $contentType */
+            $contentType = $subject;
+            $contentTypeName = $contentType->getSlug();
+        } else {
+            return false;
         }
 
+        // try to find a contenttype specific setting first
+        if ($this->contenttypePermissions) {
+            $contenTypePermissions = $this->contenttypePermissions->get($contentTypeName);
+            if ($contenTypePermissions) {
+                $roles = $contenTypePermissions->get($attribute);
+                if ($roles) {
+                    // check if user is granted any of the specified attributes/roles
+                    return $this->isGrantedAny($roles, $subject);
+                }
+            }
+        }
+
+        // if there was no specific rule for this contenttype + attribute, fall back to the default
+        $contentTypeDefaultPermissions = $this->contenttypePermissionsDefault;
+        if ($contentTypeDefaultPermissions) {
+            $roles = $contentTypeDefaultPermissions->get($attribute);
+
+            if ($roles) {
+                // check if user is granted any of the specified attributes/roles
+                return $this->isGrantedAny($roles, $subject);
+            }
+        }
+
+        // apparently there was no match -> deny!
         return false;
+
+//        // Or do we always want to have a 'complete' setup, and throw an error here?
+//        throw new \LogicException('This code should not be reached!');
     }
 
-    private function canEdit(Content $content, User $user)
+    private function isGrantedAny($attributes, $subject = null)
     {
-        return $user === $content->getAuthor();
+        foreach ($attributes as $attribute) {
+            if ($this->security->isGranted($attribute, $subject)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
