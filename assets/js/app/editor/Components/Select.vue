596e71e8fdbf6e8e5d7acf3eccfa4f7f95ed9142
Bob den Otter <bob@twokings.nl>
2020-11-23 14:04:19 +0100

    Merging mishap: This shouldn't have been in this branch

--- assets/js/app/editor/Components/Select.vue
+++ assets/js/app/editor/Components/Select.vue
@@ -89,12 +89,8 @@ export default {
             if (this.selected === null) {
                 return JSON.stringify([]);
             } else if (this.selected.map) {
-                if (typeof item !== 'undefined') {
-                    filtered = this.selected.map(item => item.key);
-                    return JSON.stringify(filtered);
-                } else {
-                    return JSON.stringify([]);
-                }
+                filtered = this.selected.map(item => item.key);
+                return JSON.stringify(filtered);
             } else {
                 return JSON.stringify([this.selected.key]);
             }
