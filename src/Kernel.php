2092377ae13dc964504173d09e9a464f29d73e98
Bob den Otter <bob@twokings.nl>
2020-11-26 23:22:55 +0100

    Revert "Update Kernel.php"
    This reverts commit 1b695e3a091d9bb3ad4f12f17a396125ae641a05.

--- src/Kernel.php
+++ src/Kernel.php
@@ -28,7 +28,6 @@ class Kernel extends BaseKernel
     public function registerBundles(): iterable
     {
         $contents = require $this->getProjectDir() . '/config/bundles.php';
-
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
