82192c522cadb3736d1ac6c2ae267304ab1d099e
Bob den Otter <bob@twokings.nl>
2020-11-29 12:17:22 +0100

    Set the Version to `4.2 beta 1`

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.1.7';
+    public const VERSION = '4.2.0 beta 1';
     public const CODENAME = '';
 
     /**
