1d30a4402cd162baaa7ea736dac65f03d32ff3d0
Bob den Otter <bob@twokings.nl>
2020-12-01 14:48:23 +0100

    Update ContentExtension.php

--- src/Api/Extensions/ContentExtension.php
+++ src/Api/Extensions/ContentExtension.php
@@ -35,6 +35,12 @@ final class ContentExtension implements QueryCollectionExtensionInterface, Query
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, ?string $operationName = null): void
     {
+        /**
+         * Note: We're not distinguishing between `viewless` and `viewless_listing` here. In the
+         * context of the API it makes no sense to say "You can get a list, but not the details"
+         * or vice versa.
+         */
+
         if ($resourceClass === Content::class) {
             $this->filterUnpublishedViewlessContent($queryBuilder);
         }
