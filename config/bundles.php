a37ce56ad0f2bdcc627dc95e16ed446fc5944594
Prou Yann <toofff.development@gmail.com>
2020-12-09 13:13:07 +0100

    Feat: remove SymfonyMakerBundle that does not seem to be in use

--- config/bundles.php
+++ config/bundles.php
@@ -14,7 +14,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true, 'local' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
