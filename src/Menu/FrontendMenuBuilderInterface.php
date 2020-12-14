b95ae05427f00f83179ec35ce13a2e708ffea224
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-12-14 12:38:39 +0100

    Localize menu with `locale: label` yaml configuration

--- src/Menu/FrontendMenuBuilderInterface.php
+++ src/Menu/FrontendMenuBuilderInterface.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Bolt\Menu;
 
+use Twig\Environment;
+
 interface FrontendMenuBuilderInterface
 {
-    public function buildMenu(?string $name): array;
+    public function buildMenu(Environment $twig, ?string $name): array;
 }
