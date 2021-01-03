335225155a2bf734860cd6f65a3d51128c22a2d9
Bob den Otter <bob@twokings.nl>
2021-01-03 10:38:20 +0100

    Better fix: Just bring in the one file with actual code

--- src/Utils/LocaleHelper.php
+++ src/Utils/LocaleHelper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bolt\Utils;
 
-use bobdenotter\flagMaster\flagMaster;
 use Bolt\Configuration\Config;
 use Bolt\Repository\ContentRepository;
 use Symfony\Component\HttpFoundation\Request;
