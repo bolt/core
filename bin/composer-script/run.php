474189c6c293125b74081e7ad8d2012caefe743c
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-01-05 11:44:36 +0100

    Add more scripts

--- bin/composer-script/run.php
+++ bin/composer-script/run.php
@@ -2,11 +2,13 @@
 
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-function run(string $command, SymfonyStyle $symfonyStyle, bool $withOutput = false): void {
+function run(string $command, SymfonyStyle $symfonyStyle, bool $withOutput = false, string $message = ''): void {
     exec($command, $output, $return);
     if ($return) {
         // Some error occurred.
-        $message = sprintf("Command '%s' failed. %s", $command, implode("\n", $output));
+        if (empty($message)) {
+            $message = sprintf("Command '%s' failed. %s", $command, implode("\n", $output));
+        }
         $symfonyStyle->error($message);
     } else {
         if ($withOutput) {
