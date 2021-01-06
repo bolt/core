85739a4573e8ca7c3c9c9d011acee5e289d9ef52
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-01-06 15:37:14 +0100

    Allow migratiosn to run from project. Check migrations in composer install

--- bin/composer-script/project-post-install-cmd.php
+++ bin/composer-script/project-post-install-cmd.php
@@ -17,5 +17,10 @@ run('php bin/console cache:clear --no-warmup --ansi', $symfonyStyle);
 run('php bin/console assets:install --ansi', $symfonyStyle);
 run('vendor/bin/security-checker security:check', $symfonyStyle);
 
+// Check for database migrations
+$migrate = "Database is out-of-date. To update the database, run `php bin/console doctrine:migrations:migrate`.";
+$migrate .= " You are strongly advised to backup your database before migrating.";
+run('php bin/console doctrine:migrations:up-to-date', $symfonyStyle, false, $migrate);
+
 // Configure the extensions
 run('php bin/console extensions:configure --ansi', $symfonyStyle);
