890816aead165aa55c758e2bab79c7559d9263b2
Bob den Otter <bob@twokings.nl>
2021-01-13 12:19:55 +0100

    Prepare release 4.2.0-beta.5

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.2.0 beta 4';
+    public const VERSION = '4.2.0 beta 5';
     public const CODENAME = 'Whoop, whoop, beta cycle!';
 
     /**
