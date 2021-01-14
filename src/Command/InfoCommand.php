a4907315554f1ec0832819dde14746f5dff32a7d
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-01-14 17:12:20 +0100

    Show warning for outdated `composer.json` for 4.2

--- src/Command/InfoCommand.php
+++ src/Command/InfoCommand.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 namespace Bolt\Command;
 
 use Bolt\Version;
+use Composer\Composer;
+use Composer\InstalledVersions;
+use Composer\Package\Package;
+use Composer\Package\PackageInterface;
+use ComposerPackages\Packages;
+use PackageVersions\Versions;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -79,8 +85,44 @@ HELP
             sprintf('Operating System: <info>%s</info> - <comment>%s</comment>', php_uname('s'), php_uname('r')),
         ]);
 
+        $this->warnOutdatedComposerJson($io);
+
         $io->text('');
 
         return 0;
+    }
+
+    private function warnOutdatedComposerJson(SymfonyStyle $io): void
+    {
+        if (Version::compare('4.2.0', '<=')) {
+            $composer = json_decode(file_get_contents('composer.json'));
+            $warning = false;
+
+            if ($composer->scripts->{'post-install-cmd'}[0] !== 'php vendor/bolt/core/bin/composer-script/project-post-install-cmd.php') {
+                $io->warning('The post-install-cmd script in composer.json is out of date.');
+                $warning = true;
+            }
+
+            if ($composer->scripts->{'pre-update-cmd'}[0] ?? null !== 'php vendor/bolt/core/bin/composer-script/project-pre-update-cmd.php') {
+                $io->warning('The pre-update-cmd script in composer.json is out of date.');
+                $warning = true;
+            }
+
+            if ($composer->scripts->{'post-update-cmd'}[0] !== 'php vendor/bolt/core/bin/composer-script/project-post-update-cmd.php') {
+                $io->warning('The post-update-cmd script in composer.json is out of date.');
+                $warning = true;
+            }
+
+            if ($composer->scripts->{'post-create-project-cmd'}[0] !== 'php vendor/bolt/core/bin/composer-script/project-post-create-project-cmd.php') {
+                $io->warning('The post-create-project-cmd script in composer.json is out of date.');
+                $warning = true;
+            }
+
+            if ($warning) {
+                $update = 'Check the update instructions at <href=https://github.com/bolt/core/discussions/2318>https://github.com/bolt/core/discussions/2318</>';
+                $io->writeln($update);
+            }
+
+        }
     }
 }
