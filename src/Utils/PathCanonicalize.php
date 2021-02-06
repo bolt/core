a10b0624d08b5866715dc5d7668b9a87d10e8c51
Bob den Otter <bob@twokings.nl>
2021-02-06 12:41:29 +0100

    Don't allow Path Traversal

---
+++ src/Utils/PathCanonicalize.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Utils;
+
+use Webmozart\PathUtil\Path;
+
+class PathCanonicalize
+{
+    public static function canonicalize(string $basePath, string $filename): string
+    {
+        $path = Path::canonicalize($basePath . DIRECTORY_SEPARATOR . $filename);
+
+        // If the path is outside the `$basePath`, we do not allow it.
+        if (mb_strpos(Path::makeRelative($path, $basePath), '..') === 0) {
+            throw new \Exception('You are not allowed to do that');
+        }
+
+        return $path;
+    }
+}
