4894db4567433f111835212754deb88b521d160a
Bob den Otter <bob@twokings.nl>
2021-02-13 16:40:28 +0100

    Prepare release 4.1.12

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.1.12';
+    public const VERSION = '4.1.13';
     public const CODENAME = '';
 
     /**
