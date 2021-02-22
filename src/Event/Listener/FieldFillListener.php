e728d6eee7a39e691e38716f684e13b0b377ab5e
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-02-22 16:52:53 +0100

    Don't break collection looping if definition changes

--- src/Event/Listener/FieldFillListener.php
+++ src/Event/Listener/FieldFillListener.php
@@ -146,6 +146,6 @@ class FieldFillListener
     {
         return collect($fields)->filter(function (Field $field) use ($definition) {
             return $definition->get('fields') && $definition->get('fields')->has($field->getName());
-        })->toArray();
+        })->values()->toArray();
     }
 }
