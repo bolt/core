1b14d8dec937a27b31faa886786304b00cef8c55
Bob den Otter <bob@twokings.nl>
2021-04-07 19:45:00 +0200

    Allow parsing of `localdate`s like `now` or `today + 1 week, 21:00`

--- src/Twig/LocaleExtension.php
+++ src/Twig/LocaleExtension.php
@@ -169,7 +169,7 @@ class LocaleExtension extends AbstractExtension
         } elseif (empty($dateTime)) {
             $dateTime = Carbon::now();
         } else {
-            $dateTime = Carbon::createFromTimeString($dateTime);
+            $dateTime = Carbon::parse($dateTime);
         }
 
         if ($format === null) {
