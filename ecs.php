2a236f5cd41d9d245467cfab351fad5095d22d55
Bob den Otter <bob@twokings.nl>
2021-04-08 17:16:09 +0200

    CS fixes ♾

--- ecs.php
+++ ecs.php
@@ -49,7 +49,7 @@ use Symplify\CodingStandard\Fixer\Strict\BlankLineAfterStrictTypesFixer;
 // Suppress `Notice:`s in ECS 8.x This is probably fixed in the 9.x versions,
 // but we can't update to that version, because it's PHP > 7.3 only.
 // See: https://github.com/bolt/core/issues/2519
-error_reporting( error_reporting() & ~E_NOTICE );
+error_reporting(error_reporting() & ~E_NOTICE);
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
