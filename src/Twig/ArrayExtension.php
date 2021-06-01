47eb981045876a0639685d481f6b91100953554f
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2021-06-01 14:57:04 +0200

    Refactor getArray

--- src/Twig/ArrayExtension.php
+++ src/Twig/ArrayExtension.php
@@ -80,14 +80,7 @@ final class ArrayExtension extends AbstractExtension
      */
     public function order(Environment $twig, $array, string $on = '-publishedAt', ?string $onSecondary = null, $locale = null): array
     {
-        if ($array instanceof Pagerfanta) {
-            $array = (array) $array->getCurrentPageResults();
-        } elseif ($array instanceof Iterator) {
-            // Special edge-case for "|filter. See #2601
-            $array = iterator_to_array($array);
-        } elseif (! is_array($array) && is_iterable($array)) {
-            $array = (array) $array;
-        }
+        $array = $this->getArray($array);
 
         if (! $locale) {
             $locale = ! empty($this->localeExtension->getHtmlLang($twig)) ?
@@ -166,7 +159,12 @@ final class ArrayExtension extends AbstractExtension
     private function getArray($array)
     {
         if ($array instanceof Pagerfanta) {
-            return (array) $array->getCurrentPageResults();
+            $array = (array) $array->getCurrentPageResults();
+        } elseif ($array instanceof Iterator) {
+            // Special edge-case for "|filter. See #2601
+            $array = iterator_to_array($array);
+        } elseif (! is_array($array) && is_iterable($array)) {
+            $array = (array) $array;
         }
 
         return $array;
