306a7b40c99f80d7c1bf6aec4e26cf317fd437a2
Bob den Otter <bob@twokings.nl>
2021-06-25 11:26:55 +0200

    Prepare release 5.0.0-beta.10

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '5.0.0 beta 9';
+    public const VERSION = '5.0.0 beta 10';
     public const CODENAME = 'Nothing to see here!';
 
     /**
