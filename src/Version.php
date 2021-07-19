aa79c8a516e42ee264759fbcb7ed40ed3b19866c
Bob den Otter <bob@twokings.nl>
2021-07-19 15:58:43 +0200

    Update Version.php

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '4.1.99.14';
+    public const VERSION = '4.2.0 beta 14';
     public const CODENAME = '';
 
     /**
