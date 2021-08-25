016f8d3c4e5606ebb3e978f7b329f61670aa83bf
Bob den Otter <bob@twokings.nl>
2021-08-25 14:08:40 +0200

    Why write more code, when you can also write less code

--- src/Command/ServerCommand.php
+++ src/Command/ServerCommand.php
@@ -9,20 +9,10 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 
 class ServerCommand extends Command
 {
-    public const SYMFONY_SERVER = 'Symfony Server';
-    public const PHP_SERVER = 'Built-in PHP Server';
-
-    /** @var string */
-    protected $ip;
-
-    /** @var int */
-    protected $port;
-
     /** @var string */
     private $publicFolder;
 
@@ -36,102 +26,41 @@ class ServerCommand extends Command
     protected function configure(): void
     {
         $this
-            ->setName('server')
+            ->setName('bolt:server')
             ->addOption('port', 'p', InputOption::VALUE_OPTIONAL, 'Preferred HTTP port rather than auto-find (default is 8000-9000')
-            ->addOption('symfony', null, InputOption::VALUE_NONE, 'Force using Symfony server')
-            ->addOption('php', null, InputOption::VALUE_NONE, 'Force using built-in PHP server')
             ->addOption('stop', null, InputOption::VALUE_NONE, 'Stop the running webserver')
-            ->setDescription("Runs built-in web-server, Symfony first, then tries PHP's")
-            ->setHelp("Runs built-in web-server, Symfony first, then tries PHP's");
+            ->setDescription("Suggest a command to run a webserver. Symfony first, then PHP's")
+            ->setHelp("Suggest a command to run a webserver. Symfony first, then PHP's");
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $io->title('Bolt Web Server');
-
-        // Ensure CLI colors are on
-        ini_set('cli_server.color', 'on');
-
         // Options
         $force_symfony = $input->getOption('symfony');
         $force_php = $input->getOption('php');
         $stop = $input->getOption('stop');
 
-        // Find PHP
-        $executableFinder = new PhpExecutableFinder();
-        $php = $executableFinder->find(false);
-
-        $this->ip = '127.0.0.1';
-        $this->port = (int) ($input->getOption('port') ?? 8000);
+        $ip = '127.0.0.1';
+        $port = (int) ($input->getOption('port') ?? 8000);
 
         // Get an open port
-        while (! $this->portAvailable($this->ip, $this->port)) {
-            $this->port++;
+        while (! $this->portAvailable($ip, $port)) {
+            $port++;
         }
 
-        // Setup the commands
         if ($stop) {
-            $symfony_cmd = ['symfony', 'server:stop', '--ansi'];
-            $php_cmd = [$php, 'bin/console', 'server:stop', '--ansi'];
+            $command = $this->stopCommand($io);
         } else {
-            $symfony_cmd = ['symfony', 'server:start', '--ansi', '--port=' . $this->port, '-d'];
-            $php_cmd = [$php, 'bin/console', 'server:start', '--ansi', $this->port];
+            $command = $this->startCommand($io);
         }
 
-        $commands = [
-            self::SYMFONY_SERVER => $symfony_cmd,
-            self::PHP_SERVER => $php_cmd,
-        ];
+        $io->comment(sprintf('You can <options=bold>%s</> a webserver by running the following command:', ($stop ? 'stop' : 'start')));
 
-        if ($force_symfony) {
-            unset($commands[self::PHP_SERVER]);
-        } elseif ($force_php) {
-            unset($commands[self::SYMFONY_SERVER]);
-        }
+        $io->text(sprintf($command, $port));
 
-        $error = 1;
-
-        foreach ($commands as $name => $command) {
-            if ($this->runProcess($name, $command, $io)) {
-                $error = 0;
-
-                break;
-            }
-        }
-
-        if ($error) {
-            $io->error('Could not start either Symfony or PHP\'s built-in webserver');
-        }
-
-        return $error;
-    }
-
-    /**
-     * @return Process
-     */
-    protected function runProcess(string $name, array $cmd, SymfonyStyle $io): ?Process
-    {
-        $process = new Process($cmd);
-        $process->setTimeout(0);
-        $process->start();
-
-        $process->wait(function ($type, $buffer) use ($io): void {
-            if (! mb_strpos($buffer, 'symfony: not found')) {
-                $io->write($buffer);
-            }
-        });
-
-        if ($name === self::PHP_SERVER && ! mb_strpos($process->getErrorOutput(), 'already been started.')) {
-            $io->success('Built-in PHP web server listening on http://' . $this->ip . ':' . $this->port . ' (PHP v' . PHP_VERSION . ')');
-        }
-
-        if ($name === self::SYMFONY_SERVER && mb_strpos($process->getErrorOutput(), 'symfony: not found')) {
-            return null;
-        }
-
-        return $process;
+        return Command::SUCCESS;
     }
 
     /**
@@ -147,5 +76,33 @@ class ServerCommand extends Command
         fclose($fp);
 
         return false;
+    }
+
+    protected function startCommand(): string
+    {
+        if ($this->hasSymfonyCommand()) {
+            return 'symfony server:start -d --port=%s';
+        }
+
+        return 'php bin/console server:start %s';
+    }
+
+    protected function stopCommand(): string
+    {
+        if ($this->hasSymfonyCommand()) {
+            return 'symfony server:stop';
+        }
+
+        return 'php bin/console server:stop';
+    }
+
+    protected function hasSymfonyCommand()
+    {
+        $process = new Process(['symfony', 'version']);
+        $process->setTimeout(0);
+        $process->start();
+
+        return $process->wait(function (): void {
+        }) === 0;
     }
 }
