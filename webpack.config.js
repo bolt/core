e15d74d4f06c25049960580208795fc400b97fb2
Jonathan Barbin <jonathan.barbin-ext@ekino.com>
2021-11-26 21:27:30 +0100

    chore(webpack): improve webpack configuration for local environment     - run encore automatically with docker     - add hash to file name for avoid cache issues (local environment)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 const WebpackBar = require('webpackbar');
 const Encore = require('@symfony/webpack-encore');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+if (!Encore.isProduction()) {
+  Encore.addPlugin(new HtmlWebpackPlugin());
+  Encore.addPlugin(new CleanWebpackPlugin());
+  Encore.configureFilenames({
+    css: '[name].[contenthash].css',
+    js: '[name].[contenthash].js',
+  });
 }
 
 Encore
