3d2b1fc9c4960c9129d52fefd4609561c6b7ec13
Vlad Ghita <vlad_micutul@yahoo.com>
2022-03-03 17:42:02 +0200

    Translate login required message.

--- src/Security/AuthenticationEntryPointRedirector.php
+++ src/Security/AuthenticationEntryPointRedirector.php
@@ -7,20 +7,24 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AuthenticationEntryPointRedirector implements AuthenticationEntryPointInterface
 {
+    private $translator;
+
     private $urlGenerator;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    public function __construct(TranslatorInterface $translator, UrlGeneratorInterface $urlGenerator)
     {
+        $this->translator = $translator;
         $this->urlGenerator = $urlGenerator;
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
     {
         // add a custom flash message and redirect to the login page
-        $request->getSession()->getFlashBag()->add('warning', 'You have to login in order to access this page.');
+        $request->getSession()->getFlashBag()->add('warning', $this->translator->trans('You have to login in order to access this page.', [], 'security'));
 
         return new RedirectResponse($this->urlGenerator->generate('bolt_login'));
     }
