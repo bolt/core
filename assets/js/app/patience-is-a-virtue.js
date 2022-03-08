dabe2c863d5d4719e70fa8ce55097cfb9139f6f2
Joossensei <jj.both03@gmail.com>
2022-03-08 09:00:18 +0100

    Change button so it doesnt reload the page

--- assets/js/app/patience-is-a-virtue.js
+++ assets/js/app/patience-is-a-virtue.js
@@ -1,31 +1,35 @@
 import $ from 'jquery';
 
-$('a[data-patience], button[data-patience]').on('click', function() {
-    const thisElement = $(this);
-    const thisIcon = thisElement.find('i');
+export function patience_virtue() {
+    $('a[data-patience], button[data-patience]').on('click', function () {
+        const thisElement = $(this);
+        const thisIcon = thisElement.find('i');
 
-    // If we're handling a form, and the form's not valid, we can stop here
-    if ($(this).attr('form') && !$('#' + $(this).attr('form'))[0].checkValidity()) {
-        return;
-    }
+        // If we're handling a form, and the form's not valid, we can stop here
+        if ($(this).attr('form') && !$('#' + $(this).attr('form'))[0].checkValidity()) {
+            return;
+        }
 
-    // Bootstrap padding / margin like `mx-2` or `pt-3`
-    const addedPadding = thisIcon.attr('class').match(/[mp][tblrxy]-[0-5]/i);
-    const newClass = 'fas fa-w fa-cog fa-spin ' + (addedPadding ? addedPadding : '');
+        // Bootstrap padding / margin like `mx-2` or `pt-3`
+        const addedPadding = thisIcon.attr('class').match(/[mp][tblrxy]-[0-5]/i);
+        const newClass = 'fas fa-w fa-cog fa-spin ' + (addedPadding ? addedPadding : '');
 
-    $(this).attr('data-original-class', thisIcon.attr('class'));
-    thisIcon.attr('class', newClass);
+        $(this).attr('data-original-class', thisIcon.attr('class'));
+        thisIcon.attr('class', newClass);
 
-    window.setTimeout(function() {
-        thisElement.attr('disabled', true);
-    }, 50);
-});
-
-window.reEnablePatientButtons = function() {
-    $('*[data-original-class]').each(function() {
-        const thisIcon = $(this).find('i');
-
-        $(this).attr('disabled', false);
-        thisIcon.attr('class', $(this).attr('data-original-class'));
+        window.setTimeout(function () {
+            thisElement.attr('disabled', true);
+        }, 50);
     });
-};
+}
+
+export function renable() {
+    window.reEnablePatientButtons = function () {
+        $('*[data-original-class]').each(function () {
+            const thisIcon = $(this).find('i');
+
+            $(this).attr('disabled', false);
+            thisIcon.attr('class', $(this).attr('data-original-class'));
+        });
+    };
+}
