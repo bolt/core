6f388a3e2f558fcbeb0fcc855f83731bc41bf608
Joossensei <jj.both03@gmail.com>
2022-03-08 11:52:10 +0100

    Kinda fix patience function

--- assets/js/app/patience-is-a-virtue.js
+++ assets/js/app/patience-is-a-virtue.js
@@ -1,35 +1,35 @@
 import $ from 'jquery';
 
 export function patience_virtue() {
-    $('a[data-patience], button[data-patience]').on('click', function () {
-        const thisElement = $(this);
-        const thisIcon = thisElement.find('i');
+    const thisElement = $('a[data-patience], button[data-patience]');
+    const thisIcon = thisElement.find('i');
 
-        // If we're handling a form, and the form's not valid, we can stop here
-        if ($(this).attr('form') && !$('#' + $(this).attr('form'))[0].checkValidity()) {
-            return;
-        }
+    console.log(thisElement);
 
-        // Bootstrap padding / margin like `mx-2` or `pt-3`
-        const addedPadding = thisIcon.attr('class').match(/[mp][tblrxy]-[0-5]/i);
-        const newClass = 'fas fa-w fa-cog fa-spin ' + (addedPadding ? addedPadding : '');
+    // If we're handling a form, and the form's not valid, we can stop here
+    if (thisElement.attr('form') && !$('#' + thisElement.attr('form'))[0].checkValidity()) {
+        return;
+    }
 
-        $(this).attr('data-original-class', thisIcon.attr('class'));
-        thisIcon.attr('class', newClass);
+    // Bootstrap padding / margin like `mx-2` or `pt-3`
+    const addedPadding = thisIcon.attr('class').match(/[mp][tblrxy]-[0-5]/i);
+    const newClass = 'fas fa-w fa-cog fa-spin ' + (addedPadding ? addedPadding : '');
 
-        window.setTimeout(function () {
-            thisElement.attr('disabled', true);
-        }, 50);
-    });
+    console.log(thisElement);
+
+    $(this).attr('data-original-class', thisIcon.attr('class'));
+    thisIcon.attr('class', newClass);
+
+    window.setTimeout(function() {
+        thisElement.attr('disabled', true);
+    }, 50);
 }
 
 export function renable() {
-    window.reEnablePatientButtons = function () {
-        $('*[data-original-class]').each(function () {
-            const thisIcon = $(this).find('i');
+    $('*[data-original-class]').each(function() {
+        const thisIcon = $(this).find('i');
 
-            $(this).attr('disabled', false);
-            thisIcon.attr('class', $(this).attr('data-original-class'));
-        });
-    };
+        $(this).attr('disabled', false);
+        thisIcon.attr('class', $(this).attr('data-original-class'));
+    });
 }
