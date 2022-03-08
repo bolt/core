4670473a7af2d9d889875ac3a4efb7c5f033e5dd
Joossensei <jj.both03@gmail.com>
2022-03-08 12:14:39 +0100

    Fixed the patience

--- assets/js/app/ajax-save.js
+++ assets/js/app/ajax-save.js
@@ -1,21 +1,20 @@
 import $ from 'jquery';
 import { patience_virtue, renable } from './patience-is-a-virtue';
 
-let i = 0;
-
 let form = $('#editcontent');
 let record_id = form.data('record');
+let element = $('button[name="save"]');
 
 record_id = JSON.stringify(record_id);
 /**
  * Start of Ajaxy solution for saving
  */
-$('button[name="save"]').click(function() {
+element.click(function() {
     $.ajax({
         type: 'POST',
         link: '/edit/' + record_id,
         data: $(form).serialize(),
-        beforeSend: patience_virtue,
-        complete: renable,
+        beforeSend: patience_virtue(element),
+        complete: { renable },
     });
 });
