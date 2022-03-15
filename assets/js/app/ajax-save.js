636b669028644b1c2a15da567da2e7361481079b
Joossensei <jj.both03@gmail.com>
2022-03-15 12:36:43 +0100

    Update ajax-save.js
    Temp commit

--- assets/js/app/ajax-save.js
+++ assets/js/app/ajax-save.js
@@ -9,19 +9,40 @@ record_id = JSON.stringify(record_id);
 /**
  * Start of Ajaxy solution for saving
  */
-element.click(function() {
-    $.ajax({
-        type: 'POST',
-        link: '/edit/' + record_id,
-        data: $(form).serialize(),
-        beforeSend: function() {
-            patience_virtue(element);
-        },
-        complete: function() {
-            renable();
-        },
-        error: function(jq, status, err) {
-            console.log(status, err);
-        },
+$(document).ready(function () {
+    let dirty = false;
+    form.on('change', function () {
+        dirty = true;
+    });
+
+    if (dirty === true) {
+        // The confirmation message is as fallback. Modern browser show their own messages.
+        const confirmationMessage =
+            'It looks like you have been editing something. ' +
+            'If you leave before saving, your changes will be lost.';
+
+        (event || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    }
+
+    element.on('click', function () {
+        $.ajax({
+            type: 'POST',
+            link: '/edit/' + record_id,
+            data: $(form).serialize(),
+            beforeSend: function () {
+                patience_virtue(element);
+            },
+            complete: function () {
+                renable();
+            },
+            error: function (jq, status, err) {
+                console.log(status, err);
+            },
+        });
+        console.log(dirty)
+        if (dirty === true) {
+            dirty = false;
+        }
     });
 });
