09e14bb5788cb60c13d7aa2c7ec73a468071d0f6
Joossensei <jj.both03@gmail.com>
2022-03-24 14:01:46 +0100

    Update ajax-save.js
    Added toast notification

--- assets/js/app/ajax-save.js
+++ assets/js/app/ajax-save.js
@@ -5,14 +5,26 @@ let form = $('#editcontent');
 let record_id = form.data('record');
 let element = $('button[name="save"]');
 
+const toastSuccess =
+    '            <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-delay="6000">\n' +
+    '                <div class="alert-success toast-header">\n' +
+    '                    <strong class="mr-auto"></strong>\n' +
+    '                    <small>Success</small>\n' +
+    '                    <button class="ml-2 mb-1 close" aria-label="Close" data-dismiss="toast" type="button">\n' +
+    '                        <span aria-hidden="true">&times;</span>\n' +
+    '                    </button>\n' +
+    '                </div>\n' +
+    '                <div class="toast-body">Content updated succesfully</div>\n' +
+    '            </div>';
+
 record_id = JSON.stringify(record_id);
 /**
  * Start of Ajaxy solution for saving
  */
 $(document).ready(function() {
-    var unsaved = false;
+    let unsaved = false;
 
-    $(':input').change(function() {
+    $(form).change(function() {
         //triggers change in all input fields including text type
         unsaved = true;
     });
@@ -24,6 +36,14 @@ $(document).ready(function() {
     }
 
     window.onbeforeunload = unloadPage;
+
+    function showToast(toastType) {
+        $(toastType).append('.admin__notifications');
+    }
+
+    function hideToast(toastType) {
+        $(toastType).detach('.admin__notifications');
+    }
 
     element.on('click', function() {
         $.ajax({
@@ -40,9 +60,8 @@ $(document).ready(function() {
                 if (!record_id) {
                     window.location.replace(data);
                 } else {
-                    $(document).ready(function() {
-                        $('.toast').toast('show');
-                    });
+                    showToast(toastSuccess);
+                    setTimeout(hideToast(toastSuccess), 5000);
                 }
             },
             error: function(jq, status, err) {
