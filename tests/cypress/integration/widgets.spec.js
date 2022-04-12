fa924e91afbe9705aaea947f92acb16eee5fb260
Joossensei <jj.both03@gmail.com>
2022-04-12 12:33:38 +0200

    Update widgets.spec.js
    It works

--- tests/cypress/integration/widgets.spec.js
+++ tests/cypress/integration/widgets.spec.js
@@ -4,7 +4,6 @@ describe('As an admin I want to see the News Widget', () => {
     it('checks if News widget exists', () => {
         cy.login();
         cy.visit('/bolt/');
-        // Temp I need cypress to fail
-        cy.get('#widget-news-widget').should('contain', 'Latest Bolt News TEST');
+        cy.get('#widget-news-widget').should('contain', 'Latest Bolt News');
     })
 });
