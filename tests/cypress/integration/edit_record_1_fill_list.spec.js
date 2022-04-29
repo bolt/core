7f47ddc4106d2dbcfd2d6f776b83fbf766460c39
Joossensei <jj.both03@gmail.com>
2022-04-29 12:19:39 +0200

    Update edit_record_1_fill_list.spec.js
    Trying to fix this one

--- tests/cypress/integration/edit_record_1_fill_list.spec.js
+++ tests/cypress/integration/edit_record_1_fill_list.spec.js
@@ -62,12 +62,11 @@ describe('As an Admin I want to fill in an imagelist and filelist', { retries: 0
         cy.get('a[id="files-tab"]').click({ force: true}) ;
         cy.get("label[for='field-filelist']").should('contain', 'Filelist');
 
-        cy.get('button[name="file-upload-dropdown"]').eq(1).scrollIntoView();
-        cy.get('button[name="file-upload-dropdown"]').eq(1).click({ force: true}) ;
+        cy.get('button[name="file-upload-dropdown"]').eq(2).scrollIntoView();
+        cy.get('button[name="file-upload-dropdown"]').eq(2).click({ force: true}) ;
         cy.get('button[class="btn dropdown-item"]').find('i[class="fas fa-fw fa-th"]').eq(6).click({ force: true}) ;
 
         cy.get('div[class="modal-dialog"]').its('length').should('eq', 1);
-        cy.wait(10000);
         cy.get('div[class="modal-dialog"]').find('input[value="bolt4.pdf"]').click({ force: true });
         cy.get('button[id="modalButtonAccept"]').click({ force: true });
         cy.wait(1000);
