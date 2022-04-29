9dda14ccb97b90feb8ae6388b8d72a79258fa212
Joossensei <jj.both03@gmail.com>
2022-04-29 17:10:21 +0200

    Update modal.js
    Revert and build a failsafe

--- assets/js/app/modal.js
+++ assets/js/app/modal.js
@@ -1,6 +1,10 @@
 export function resetModalContent(labels) {
-    let defaultContent =
-        `
+    var resourcesModal = document.getElementById('resourcesModal');
+    if (labels.modal_button_deny && labels.modal_button_save) {
+        let deny_button = labels.modal_button_deny;
+        let save_button = labels.modal_button_save;
+        let defaultContent =
+            `
                 <div class="modal-header">
                     <h5 class="modal-title" id="resourcesModalLabel">
                         <div class="spinner-border" role="status">
@@ -16,17 +20,49 @@ export function resetModalContent(labels) {
                 </div>
                 <div class="modal-footer">
                     <button data-modal-button-deny="` +
-        labels['modal_button_deny'] +
-        `" id="modalButtonDeny" type="button" class="btn btn-secondary" data-bs-dismiss="modal">` +
-        labels['modal_button_deny'] +
-        `</button>
+            deny_button +
+            `" id="modalButtonDeny" type="button" class="btn btn-secondary" data-bs-dismiss="modal">` +
+            deny_button +
+            `</button>
                     <button data-modal-button-accept="` +
-        labels['modal_button_save'] +
-        `" id="modalButtonAccept" type="button" class="btn btn-primary" data-bs-dismiss="modal">` +
-        labels['modal_button_save'] +
-        `</button>
+            save_button +
+            `" id="modalButtonAccept" type="button" class="btn btn-primary" data-bs-dismiss="modal">` +
+            save_button +
+            `</button>
                 </div>
             `;
-    var resourcesModal = document.getElementById('resourcesModal');
-    resourcesModal.querySelector('.modal-content').innerHTML = defaultContent;
+        resourcesModal.querySelector('.modal-content').innerHTML = defaultContent;
+    } else {
+        let deny_button = 'Close';
+        let save_button = 'Save';
+        let defaultContent =
+            `
+                <div class="modal-header">
+                    <h5 class="modal-title" id="resourcesModalLabel">
+                        <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button data-modal-button-deny="` +
+            deny_button +
+            `" id="modalButtonDeny" type="button" class="btn btn-secondary" data-bs-dismiss="modal">` +
+            deny_button +
+            `</button>
+                    <button data-modal-button-accept="` +
+            save_button +
+            `" id="modalButtonAccept" type="button" class="btn btn-primary" data-bs-dismiss="modal">` +
+            save_button +
+            `</button>
+                </div>
+            `;
+        resourcesModal.querySelector('.modal-content').innerHTML = defaultContent;
+    }
 }
