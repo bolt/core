519d090faf64c3714f0f8a3a50a0c81dc1645a84
Néstor de Dios Fernández <nestordedios@outlook.es>
2022-06-09 16:23:03 +0200

    Keep save and cancel buttons for modals that need it

--- assets/js/app/modal.js
+++ assets/js/app/modal.js
@@ -1,6 +1,8 @@
 export function resetModalContent(labels) {
     var resourcesModal = document.getElementById('resourcesModal');
     if (labels.modal_button_deny && labels.modal_button_save) {
+        let deny_button = labels.modal_button_deny;
+        let save_button = labels.modal_button_save;
         let defaultContent =
             `
                 <div class="modal-header">
@@ -15,10 +17,24 @@ export function resetModalContent(labels) {
                     <div class="spinner-border" role="status">
                         <span class="visually-hidden">Loading...</span>
                     </div>
+                </div>
+                <div class="modal-footer">
+                    <button data-modal-button-deny="` +
+            deny_button +
+            `" id="modalButtonDeny" type="button" class="btn btn-secondary" data-bs-dismiss="modal">` +
+            deny_button +
+            `</button>
+                    <button data-modal-button-accept="` +
+            save_button +
+            `" id="modalButtonAccept" type="button" class="btn btn-primary" data-bs-dismiss="modal">` +
+            save_button +
+            `</button>
                 </div>
             `;
         resourcesModal.querySelector('.modal-content').innerHTML = defaultContent;
     } else {
+        let deny_button = 'Close';
+        let save_button = 'Save';
         let defaultContent =
             `
                 <div class="modal-header">
@@ -33,6 +49,18 @@ export function resetModalContent(labels) {
                     <div class="spinner-border" role="status">
                         <span class="visually-hidden">Loading...</span>
                     </div>
+                </div>
+                <div class="modal-footer">
+                    <button data-modal-button-deny="` +
+            deny_button +
+            `" id="modalButtonDeny" type="button" class="btn btn-secondary" data-bs-dismiss="modal">` +
+            deny_button +
+            `</button>
+                    <button data-modal-button-accept="` +
+            save_button +
+            `" id="modalButtonAccept" type="button" class="btn btn-primary" data-bs-dismiss="modal">` +
+            save_button +
+            `</button>
                 </div>
             `;
         resourcesModal.querySelector('.modal-content').innerHTML = defaultContent;
