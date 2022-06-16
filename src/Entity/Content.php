34c88ef93a82e04411e669f36e0b4397522aa2b9
Bob den Otter <bob@twokings.nl>
2022-06-16 11:58:43 +0200

    Update Content.php

--- src/Entity/Content.php
+++ src/Entity/Content.php
@@ -735,10 +735,11 @@ class Content
             $dateTime->setTimezone($utc);
         }
 
-        // Prevent dates before the year 0, because MySQL chokes on those
-        if ((int) $dateTime->format("Y") < 1) {
-            $dateTime->setDate(0, 1, 1);
+        // Prevent dates before the year `0000`, because MySQL chokes on those
+        if ($dateTime instanceof \DateTime && (int) $dateTime->format("Y") < 1) {
+            $dateTime = null;
         }
+
 
         return $dateTime;
     }
