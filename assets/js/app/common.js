0b2bd5b6b1363136bc5a1a75a6f437cb0ff51dcb
Néstor de Dios Fernández <nestordedios@outlook.es>
2022-06-29 13:42:34 +0200

    Fix aside menu displaying on page load for small screens

--- assets/js/app/common.js
+++ assets/js/app/common.js
@@ -23,12 +23,14 @@ $(document).ready(function() {
         if ($('.admin__sidebar').hasClass('admin__sidebar--is-collapsed')) {
             $('.admin__sidebar')
                 .addClass('admin__sidebar--is-expanded')
-                .removeClass('admin__sidebar--is-collapsed');
+                .removeClass('admin__sidebar--is-collapsed')
+                .removeClass('d-xs-none');
             $(this).toggleClass('is-active');
         } else {
             $('.admin__sidebar')
                 .addClass('admin__sidebar--is-collapsed')
-                .removeClass('admin__sidebar--is-expanded');
+                .removeClass('admin__sidebar--is-expanded')
+                .removeClass('d-xs-none');
             $(this).toggleClass('is-active');
         }
     });
