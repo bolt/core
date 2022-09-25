332fd22d64b2f818ba3cfaab98266796d4ca8535
Abdellah Ramadan <ramadanabdel24@gmail.com>
2022-09-25 17:27:21 +0100

    Fix #3112. Add id '1' to the first existing admin or if not available to the current user.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -129,6 +129,11 @@ class User implements UserInterface, \Serializable, PasswordAuthenticatedUserInt
         return $this->id;
     }
 
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
     public function setDisplayName(?string $displayName): void
     {
         $this->displayName = $displayName;
@@ -249,7 +254,7 @@ class User implements UserInterface, \Serializable, PasswordAuthenticatedUserInt
     {
         return serialize($this->__serialize());
     }
-    
+
     public function __serialize(): array
     {
         return [$this->id, $this->username, $this->password];
@@ -262,7 +267,7 @@ class User implements UserInterface, \Serializable, PasswordAuthenticatedUserInt
     {
         $this->__unserialize(unserialize($serialized, ['allowed_classes' => false]));
     }
-    
+
     public function __unserialize(array $data): void
     {
         // add $this->salt too if you don't use Bcrypt or Argon2i
