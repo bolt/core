7138f4eb9a49f1d5439e6273fe9c432a53d9fe8e
Bob den Otter <bob@twokings.nl>
2023-01-20 11:22:08 +0100

    Prepare release 5.1.20

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '5.1.19';
+    public const VERSION = '5.1.20';
     public const CODENAME = '';
 
     /**
