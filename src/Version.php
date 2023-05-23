fd6f250ebb2e67707caa71b45bba8e035118bd51
Bob den Otter <bobdenotter@gmail.com>
2023-05-23 14:50:59 +0200

    Prepare release 5.2.0-beta.18

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '5.2.0 beta 17';
+    public const VERSION = '5.2.0 beta 18';
     public const CODENAME = '';
 
     /**
