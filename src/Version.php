73bdc4233e2df7ac4d17d5fa0e62ece788373e0c
Bob den Otter <bobdenotter@gmail.com>
2023-09-26 11:48:44 +0200

    Update Version.php

--- src/Version.php
+++ src/Version.php
@@ -23,7 +23,7 @@ final class Version
      *   Stable — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    public const VERSION = '5.2.0 beta 21';
+    public const VERSION = '5.2.0 beta 22';
     public const CODENAME = '';
 
     /**
