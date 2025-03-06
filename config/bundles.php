43a00406b59a2d55db927a1b8a180a3cc00a6b52
Jean-Baptiste Nahan <814683+macintoshplus@users.noreply.github.com>
2025-03-06 15:20:03 +0100

    :arrow_up: Require Symfony 5.4 and remode web server bundle

--- config/bundles.php
+++ config/bundles.php
@@ -18,7 +18,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'local' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
