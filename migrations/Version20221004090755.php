f03c82321032d2ebc6a222dec438ae03693d6585
macintoshplus <814683+macintoshplus@users.noreply.github.com>
2025-03-07 21:03:43 +0100

    issue #3540 add two field lost in database migration.

--- migrations/Version20221004090755.php
+++ migrations/Version20221004090755.php
@@ -34,6 +34,16 @@ final class Version20221004090755 extends AbstractMigration
         if (! $userTable->hasColumn('about')) {
             $userTable->addColumn('about', 'string', ['notnull' => false, 'length' => 1024]);
         }
+
+        $contentTable = $schema->getTable($this->tablePrefix . '_content');
+
+        if (! $contentTable->hasColumn('title')) {
+            $contentTable->addColumn('title', 'string', ['notnull' => false, 'length' => 191]);
+        }
+        if (! $contentTable->hasColumn('list_format')) {
+            $contentTable->addColumn('list_format', 'string', ['notnull' => false, 'length' => 191]);
+        }
+
     }
 
     public function down(Schema $schema): void
@@ -41,5 +51,10 @@ final class Version20221004090755 extends AbstractMigration
         $userTable = $schema->getTable($this->tablePrefix . '_user');
 
         $userTable->dropColumn('about');
+
+        $contentTable = $schema->getTable($this->tablePrefix . '_content');
+
+        $contentTable->dropColumn('title');
+        $contentTable->dropColumn('list_format');
     }
 }
