919c17cfbd17868a4d6edd4c9eec47f153d100a6
Bob van de Vijver <bobvandevijver@hotmail.com>
2025-07-20 15:31:43 +0200

    Fix token parser test

--- tests/php/Twig/TokenParserTestCase.php
+++ tests/php/Twig/TokenParserTestCase.php
@@ -22,9 +22,9 @@ abstract class TokenParserTestCase extends \PHPUnit\Framework\TestCase
     protected function getParser(TokenStream $tokenStream, AbstractTokenParser $testParser): Parser
     {
         $env = new Environment($this->getMockBuilder(LoaderInterface::class)->getMock());
+        $env->addTokenParser($testParser);
         $parser = new Parser($env);
         $parser->setParent(new Node());
-        $env->addTokenParser($testParser);
 
         $p = new \ReflectionProperty($parser, 'stream');
         $p->setAccessible(true);
