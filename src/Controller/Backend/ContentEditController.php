a1e3b1c0785b32e4d715b62e0b925f2315b44ba4
Bob van de Vijver <bobvandevijver@hotmail.com>
2025-11-05 12:18:33 +0100

    Enable symfont code quality rector set

--- src/Controller/Backend/ContentEditController.php
+++ src/Controller/Backend/ContentEditController.php
@@ -200,7 +200,7 @@ class ContentEditController extends TwigAwareController implements BackendZoneIn
                     'title' => $content->getExtras()['title'],
                     'modified' => $modified,
                 ],
-                200
+                Response::HTTP_OK
             );
         }
 
