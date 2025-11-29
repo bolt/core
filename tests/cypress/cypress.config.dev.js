44aa3d25d4d74e56007e24b3b1c80b96f9b6c5df
Bob van de Vijver <bobvandevijver@hotmail.com>
2025-11-29 17:57:12 +0100

    Upgrade cypress version to latest

---
+++ tests/cypress/cypress.config.dev.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'http://127.0.0.1:8088',
+        specPattern: 'tests/cypress/integration/**/*.spec.js',
+        supportFile: 'tests/cypress/support/index.js',
+        screenshotsFolder: 'tests/cypress/evidence/screenshots',
+        video: true,
+        videosFolder: 'tests/cypress/evidence/videos',
+        viewportWidth: 1920,
+        viewportHeight: 1080,
+        scrollBehavior: false,
+    },
+});
