0732e03db64ee74548d4a0e386fab76209d85b3a
Bob van de Vijver <bobvandevijver@hotmail.com>
2025-11-29 21:01:26 +0100

    Fix cypress tests with updates API Platform

--- tests/cypress/integration/backend_api.spec.js
+++ tests/cypress/integration/backend_api.spec.js
@@ -3,7 +3,7 @@
 describe('As an admin, I want to see the API documentation page', () => {
     it('checks that the API page exists', () => {
         cy.login();
-        cy.visit('/bolt/api');
+        cy.visit('/bolt/api', { headers: { Accept: 'text/html' } });
         cy.get('.admin__header--title').should('contain', 'Bolt API');
     });
 });
